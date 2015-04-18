fc0c3e7168b080960fba452e8bba3c670ca1bb2b
Stefan Kracht <stefankracht@gmail.com>
2015-04-18 08:26:23 +0200

    merge rails and spec helper

--- spec/rails_helper.rb
+++
@@ -1,8 +0,0 @@
-ENV['RAILS_ENV'] ||= 'test'
-require 'spec_helper'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rspec/rails'
-
-RSpec.configure do |config|
-  config.infer_spec_type_from_file_location!
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,9 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec/rails'
+
 RSpec.configure do |config|
+	config.infer_spec_type_from_file_location!
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
