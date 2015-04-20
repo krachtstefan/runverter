7fed8cab995462f12b9e628a5ae3d7186b9520ec
Stefan Kracht <stefankracht@gmail.com>
2015-04-20 16:15:19 +0200

    add qunit to asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( qunit.css )
+Rails.application.config.assets.precompile += %w( test_helper.css )
+Rails.application.config.assets.precompile += %w( qunit.js )
+Rails.application.config.assets.precompile += %w( test_helper.js )
