65b3f1173fca2cce0025603eab9ef971aad63a6b
Stefan Kracht <stefankracht@gmail.com>
2015-04-24 12:24:49 +0200

    basic test helper

--- test/javascripts/test_helper.js
+++ test/javascripts/test_helper.js
@@ -2,5 +2,13 @@
 //= require_self
 //= require_tree .
 
+
+document.write('<div id="ember-testing-container"><div id="app"></div></div>');
+document.write('<style>#ember-testing-container { position: absolute; background: white; bottom: 0; right: 0; width: 640px; height: 384px; overflow: auto; z-index: 9999; border: 1px solid #ccc; } #ember-testing { zoom: 50%; }</style>');
+
 Runverter.setupForTesting();
 Runverter.injectTestHelpers();
+
+function exists(selector) {
+  return !!find(selector).length;
+}
