a654e7713f43365c9335b9d4d815684a8a4ddc94
Stefan Kracht <stefankracht@gmail.com>
2015-04-24 09:39:24 +0200

    add time and lenght value to run instance

--- app/assets/javascripts/routes/pace_calculator.js
+++ app/assets/javascripts/routes/pace_calculator.js
@@ -1,5 +1,8 @@
 Runverter.PaceCalculatorRoute = Ember.Route.extend({
   model: function() {
-    return this.store.createRecord(Runverter.Run, {});
+    return this.store.createRecord(Runverter.Run, {
+    	timeSec : 60,
+    	lenghtM : 42195,
+    });
   }
 })
