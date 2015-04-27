89939ffcbb5c0d8bd12410639c4068699b4f2f02
Stefan Kracht <stefankracht@gmail.com>
2015-04-27 14:32:54 +0200

    convert most of the run model properties to computed properties

--- app/assets/javascripts/models/run.js
+++ app/assets/javascripts/models/run.js
@@ -1,18 +1,52 @@
 Runverter.Run = DS.Model.extend({
+
 	timeSec : null,
-	timeArr : [],
+	timeArr : function(){
+		return [];
+	}.property('timeSec'),
+
 
 	lenghtM : null,
-	lengthMiles : null,
-	lengthKm : null,
+
+	lengthMiles : function(){
+		return null;
+	}.property('lenghtM'),
+
+	lengthKm : function(){
+		return null;
+	}.property('lenghtM'),
+
 	
-	paceMinPerKm : null,
-	paceMinPerKmArr : [],
-	paceMinPerMile : null,
-	paceMinPerMileArr : [],
+	paceMinPerKm : function(){
+		return null;
+	}.property('timeSec', 'lenghtM'),
+
+	paceMinPerKmArr : function(){
+		return [];
+	}.property('timeSec', 'lenghtM'),
 	
-	speedKmHour : null,
-	speedKmHourArr : [],
-	speedMilesHour : null,
-	speedMilesHourArr : []
+	paceMinPerMile  : function(){
+		return null;
+	}.property('timeSec', 'lenghtM'),
+
+	paceMinPerMileArr : function(){
+		return [];
+	}.property('timeSec', 'lenghtM'),
+	
+
+	speedKmHour : function(){
+		return null;
+	}.property('timeSec', 'lenghtM'),
+
+	speedKmHourArr : function(){
+		return [];
+	}.property('timeSec', 'lenghtM'),
+
+	speedMilesHour : function(){
+		return null;
+	}.property('timeSec', 'lenghtM'),
+
+	speedMilesHourArr : function(){
+		return [];
+	}.property('timeSec', 'lenghtM'),
 })
