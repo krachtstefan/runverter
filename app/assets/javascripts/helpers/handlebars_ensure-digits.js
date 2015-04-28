e07a21140d2370945689dafdab00c2a9ac3dec8c
Stefan Kracht <stefankracht@gmail.com>
2015-04-28 11:08:49 +0200

    add ensure digits handlebars helper

---
+++ app/assets/javascripts/helpers/handlebars_ensure-digits.js
@@ -0,0 +1,11 @@
+// ensures value[int] to have at least digits[int]
+// use it to convert 1 to "01"
+Ember.Handlebars.helper('ensure-digits', function(value, digits) {
+  digits = typeof digits == 'number' ? digits : 2;
+  value = value.toString();
+  zeroCount = digits-value.length;
+  for(var i = 0; i < zeroCount;  i++) {
+  	value = "0"+value;
+	}
+  return value
+});
