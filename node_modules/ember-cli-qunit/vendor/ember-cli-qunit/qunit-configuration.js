380c841018b560cd5f338cfb4c0c8f2bca582bad
Stefan Kracht <stefankracht@gmail.com>
2015-05-08 10:23:00 +0200

    ember new runverter

---
+++ node_modules/ember-cli-qunit/vendor/ember-cli-qunit/qunit-configuration.js
@@ -0,0 +1,22 @@
+/* globals jQuery,QUnit */
+
+QUnit.config.autostart = false;
+QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container'});
+QUnit.config.urlConfig.push({ id: 'nojshint', label: 'Disable JSHint'});
+QUnit.config.urlConfig.push({ id: 'doccontainer', label: 'Doc test pane'});
+
+if (QUnit.notifications) {
+  QUnit.notifications({
+    icons: {
+      passed: '/assets/passed.png',
+      failed: '/assets/failed.png'
+    }
+  });
+}
+
+jQuery(document).ready(function() {
+  var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
+  var containerPosition = QUnit.urlParams.doccontainer ? 'absolute' : 'relative';
+  document.getElementById('ember-testing-container').style.visibility = containerVisibility;
+  document.getElementById('ember-testing-container').style.position = containerPosition;
+});
