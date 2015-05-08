a8ec6f171878feb505654300c4d4262bb9b02cd7
Stefan Kracht <stefankracht@gmail.com>
2015-05-08 11:37:58 +0200

    create run record

---
+++ app/routes/pace_calculator.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('run', {
+    	timeSec : 3600*4,
+    	lenghtM : 42195,
+    });
+  }
+});
