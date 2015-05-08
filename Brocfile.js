832e2bb20ea8ed3a72835baf36df6e71473c6216
Stefan Kracht <stefankracht@gmail.com>
2015-05-08 15:26:36 +0200

    add uikit to brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -3,4 +3,6 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
+app.import('bower_components/uikit/css/uikit.min.css');
+app.import('bower_components/uikit/js/uikit.min.js');
 module.exports = app.toTree();
