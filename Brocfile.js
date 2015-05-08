e57f6568f36d58380ece983b5f6c534a3247112a
Stefan Kracht <stefankracht@gmail.com>
2015-05-08 15:39:03 +0200

    import localstorage adapter

--- Brocfile.js
+++ Brocfile.js
@@ -3,6 +3,7 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
+app.import("bower_components/ember-localstorage-adapter/localstorage_adapter.js");
 app.import('bower_components/uikit/css/uikit.min.css');
 app.import('bower_components/uikit/js/uikit.min.js');
 module.exports = app.toTree();
