4dd7735cb7c4180fdeee4047c37153c040b240e1
Stefan Kracht <stefankracht@gmail.com>
2015-05-10 13:11:43 +0200

    add timeHr tests

--- tests/unit/models/run-test.js
+++ tests/unit/models/run-test.js
@@ -25,3 +25,18 @@ test('timeMin can round up', function(assert) {
 	var run = this.subject({timeSec : 2614, lenghtM : 12345});
   assert.equal(run.get("timeMin"), 43.57);
 });
+
+test('timeHr property is calculated from timeSec', function(assert) {
+	var run = this.subject({timeSec : 14400, lenghtM : 12345});
+  assert.equal(run.get("timeHr"), 4);
+});
+
+test('timeHr can round down', function(assert) {
+	var run = this.subject({timeSec : 14560, lenghtM : 12345});
+  assert.equal(run.get("timeHr"), 4.04);
+});
+
+test('timeHr can round up', function(assert) {
+	var run = this.subject({timeSec : 14860, lenghtM : 12345});
+  assert.equal(run.get("timeHr"), 4.13);
+});
