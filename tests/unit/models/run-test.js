127e5b726a5172de46c530855d5bdc7bbb5c12d8
Stefan Kracht <stefankracht@gmail.com>
2015-05-10 08:51:54 +0200

    add timeMin tests

--- tests/unit/models/run-test.js
+++ tests/unit/models/run-test.js
@@ -10,3 +10,18 @@ test('run is a valid ember data Model', function(assert) {
   assert.ok(run);
   assert.ok(run instanceof DS.Model);
 });
+
+test('timeMin property is calculated from timeSec', function(assert) {
+	var run = this.subject({timeSec : 3600, lenghtM : 12345});
+  assert.equal(run.get("timeMin"), 60);
+});
+
+test('timeMin can round down', function(assert) {
+	var run = this.subject({timeSec : 2612, lenghtM : 12345});
+  assert.equal(run.get("timeMin"), 43.53);
+});
+
+test('timeMin can round up', function(assert) {
+	var run = this.subject({timeSec : 2614, lenghtM : 12345});
+  assert.equal(run.get("timeMin"), 43.57);
+});
