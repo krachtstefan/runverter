80abc22c8411e96200011da35ed73c10aa7d8825
Stefan Kracht <stefankracht@gmail.com>
2015-05-10 13:25:13 +0200

    don't set lenghtM in run tests since this property is not needed

--- tests/unit/models/run-test.js
+++ tests/unit/models/run-test.js
@@ -12,31 +12,31 @@ test('run is a valid ember data Model', function(assert) {
 });
 
 test('timeMin property is calculated from timeSec', function(assert) {
-	var run = this.subject({timeSec : 3600, lenghtM : 12345});
+	var run = this.subject({timeSec : 3600});
   assert.equal(run.get("timeMin"), 60);
 });
 
 test('timeMin can round down', function(assert) {
-	var run = this.subject({timeSec : 2612, lenghtM : 12345});
+	var run = this.subject({timeSec : 2612});
   assert.equal(run.get("timeMin"), 43.53);
 });
 
 test('timeMin can round up', function(assert) {
-	var run = this.subject({timeSec : 2614, lenghtM : 12345});
+	var run = this.subject({timeSec : 2614});
   assert.equal(run.get("timeMin"), 43.57);
 });
 
 test('timeHr property is calculated from timeSec', function(assert) {
-	var run = this.subject({timeSec : 14400, lenghtM : 12345});
-  assert.equal(run.get("timeHr"), 4);
+	var run = this.subject({timeSec : 14400});
+ 	assert.equal(run.get("timeHr"), 4);
 });
 
 test('timeHr can round down', function(assert) {
-	var run = this.subject({timeSec : 14560, lenghtM : 12345});
+	var run = this.subject({timeSec : 14560});
   assert.equal(run.get("timeHr"), 4.04);
 });
 
 test('timeHr can round up', function(assert) {
-	var run = this.subject({timeSec : 14860, lenghtM : 12345});
+	var run = this.subject({timeSec : 14860});
   assert.equal(run.get("timeHr"), 4.13);
 });
