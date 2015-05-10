af8a1cf93bccfbdd556dcc7868ab791e13276fe1
Stefan Kracht <stefankracht@gmail.com>
2015-05-10 14:12:02 +0200

    add some comments to run test

--- tests/unit/models/run-test.js
+++ tests/unit/models/run-test.js
@@ -11,6 +11,7 @@ test('run is a valid ember data Model', function(assert) {
   assert.ok(run instanceof DS.Model);
 });
 
+// timeMin
 test('timeMin property is calculated from timeSec', function(assert) {
 	var run = this.subject({timeSec : 3600});
   assert.equal(run.get("timeMin"), 60);
@@ -26,6 +27,7 @@ test('timeMin can round up', function(assert) {
   assert.equal(run.get("timeMin"), 43.57);
 });
 
+// timeHr
 test('timeHr property is calculated from timeSec', function(assert) {
 	var run = this.subject({timeSec : 14400});
  	assert.equal(run.get("timeHr"), 4);
@@ -41,21 +43,25 @@ test('timeHr can round up', function(assert) {
   assert.equal(run.get("timeHr"), 4.13);
 });
 
+// timeStackSec
 test('timeStackSec property is calculated from timeSec', function(assert) {
 	var run = this.subject({timeSec : 62});
  	assert.equal(run.get("timeStackSec"), 2);
 });
 
+// timeStackMin
 test('timeStackMin property is calculated from timeSec', function(assert) {
 	var run = this.subject({timeSec : 145});
  	assert.equal(run.get("timeStackMin"), 2);
 });
 
+// timeStackHr
 test('timeStackHr property is calculated from timeSec', function(assert) {
 	var run = this.subject({timeSec : 20000});
  	assert.equal(run.get("timeStackHr"), 5);
 });
 
+// lengthMi
 test('lengthMi property is calculated from lenghtM', function(assert) {
 	var run = this.subject({lenghtM : 1609.344});
  	assert.equal(run.get("lengthMi"), 1);
@@ -66,11 +72,13 @@ test('lengthMi has 4 digit precision', function(assert) {
  	assert.equal(run.get("lengthMi"), 7.4565);
 });
 
+// lengthMiStackMi
 test('lengthMiStackMi property is calculated from lenghtM', function(assert) {
 	var run = this.subject({lenghtM : 1800});
  	assert.equal(run.get("lengthMiStackMi"), 1);
 });
 
+// lengthMiStackM
 test('lengthMiStackM property is calculated from lenghtM and can round up', function(assert) {
 	var run = this.subject({lenghtM : 1712});
  	assert.equal(run.get("lengthMiStackM"), 06);
