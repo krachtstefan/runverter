11a27448c9fa45d7d591b567e923eddf0fadee1f
Stefan Kracht <stefankracht@gmail.com>
2015-07-20 16:57:19 +0200

    add speedKmHrStackKm setter

--- app/models/run.js
+++ app/models/run.js
@@ -270,9 +270,18 @@ export default DS.Model.extend({
 
 	/**
 	 * lengthKmStackKm is used to create a view like 12,34
-	 * @return {number} km stack of the speed
+	 * if arguments are passed, they are used as a setter for this computed property 
+	 * 
+	 * @param  {string} 								propertyName 	if defined, it will be speedKmHrStackKm
+	 * @param  {Object|string|number} 	value        	new value of speedKmHrStackKm
+	 * @return {number} 															km stack of the speed
 	 */
-	speedKmHrStackKm : function(){
+	speedKmHrStackKm : function(propertyName, value) {
+		if (arguments.length > 1) {
+    	var previousValue = this.get("speedKmHrStackKm");
+    	value = +Math.round(value) || 0; // convert to number or set to 0
+    	this.set("speedKmHr", parseFloat(this.get('speedKmHr'))+(value-previousValue));
+		}
 		return parseInt(this.get("speedKmHr"));
 	}.property('speedKmHr'),
 
