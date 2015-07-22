9b87742c6f441bae17997818d98b06fbf30919ca
Stefan Kracht <stefankracht@gmail.com>
2015-07-22 11:27:08 +0200

    rearrange order of tests

--- tests/unit/models/run-test.js
+++ tests/unit/models/run-test.js
@@ -11,50 +11,6 @@ test('run is a valid ember data Model', function(assert) {
   assert.ok(run instanceof DS.Model);
 });
 
-// timeMin
-test('timeMin property is calculated from timeSec', function(assert) {
-	var run = this.subject({timeSec : 3600});
-  assert.strictEqual(run.get("timeMin"), "60.0000");
-});
-
-test('timeMin can round down', function(assert) {
-	var run = this.subject({timeSec : 2612});
-  assert.strictEqual(run.get("timeMin"), "43.5333");
-});
-
-test('timeMin can round up', function(assert) {
-	var run = this.subject({timeSec : 2614});
-  assert.strictEqual(run.get("timeMin"), "43.5667");
-});
-
-test('timeMin setter changes timeMin', function(assert) {
-	var run = this.subject();
-	run.set("timeMin", "100");
-	assert.strictEqual(run.get("timeMin"), "100.0000");
-});
-
-test('timeMin setter can handle floats', function(assert) {
-	var run = this.subject();
-	run.set("timeMin", "100.5");
-	assert.strictEqual(run.get("timeMin"), "100.5000");
-	run.set("timeMin", 50.5);
-	assert.strictEqual(run.get("timeMin"), "50.5000");
-});
-
-test('timeMin setter also works with integer', function(assert) {
-	var run = this.subject();
-	run.set("timeMin", 100);
-	assert.strictEqual(run.get("timeMin"), "100.0000");
-});
-
-test('timeMin setter changes timeSec', function(assert) {
-	var run = this.subject();
-	run.set("timeMin", "12");
-	assert.strictEqual(run.get("timeSec"), 720);
-	run.set("timeMin", "12.123");
-	assert.strictEqual(run.get("timeSec"), 727.38);
-});
-
 // timeHr
 test('timeHr property is calculated from timeSec', function(assert) {
 	var run = this.subject({timeSec : 14400});
@@ -99,74 +55,48 @@ test('timeHr setter changes timeSec', function(assert) {
 	assert.strictEqual(run.get("timeSec"), 7642.8);
 });
 
-// timeStackSec
-test('timeStackSec property is calculated from timeSec', function(assert) {
-	var run = this.subject({timeSec : 62});
- 	assert.strictEqual(run.get("timeStackSec"), 2);
+// timeMin
+test('timeMin property is calculated from timeSec', function(assert) {
+	var run = this.subject({timeSec : 3600});
+  assert.strictEqual(run.get("timeMin"), "60.0000");
 });
 
-test('timeStackSec setter changes timeStackSec', function(assert) {
+test('timeMin can round down', function(assert) {
+	var run = this.subject({timeSec : 2612});
+  assert.strictEqual(run.get("timeMin"), "43.5333");
+});
+
+test('timeMin can round up', function(assert) {
+	var run = this.subject({timeSec : 2614});
+  assert.strictEqual(run.get("timeMin"), "43.5667");
+});
+
+test('timeMin setter changes timeMin', function(assert) {
 	var run = this.subject();
-	run.set("timeStackSec", "10");
-	assert.strictEqual(run.get("timeStackSec"), 10);
+	run.set("timeMin", "100");
+	assert.strictEqual(run.get("timeMin"), "100.0000");
 });
 
-test('timeStackSec setter can handle floats', function(assert) {
+test('timeMin setter can handle floats', function(assert) {
 	var run = this.subject();
-	run.set("timeStackSec", "2.2");
-	assert.strictEqual(run.get("timeStackSec"), 2);
-	run.set("timeStackSec", 2.5);
-	assert.strictEqual(run.get("timeStackSec"), 3);
+	run.set("timeMin", "100.5");
+	assert.strictEqual(run.get("timeMin"), "100.5000");
+	run.set("timeMin", 50.5);
+	assert.strictEqual(run.get("timeMin"), "50.5000");
 });
 
-test('timeStackSec setter also works with integer', function(assert) {
+test('timeMin setter also works with integer', function(assert) {
 	var run = this.subject();
-	run.set("timeStackSec", 2);
-	assert.strictEqual(run.get("timeStackSec"), 2);
+	run.set("timeMin", 100);
+	assert.strictEqual(run.get("timeMin"), "100.0000");
 });
 
-test('timeStackSec setter influences all time related properties', function(assert) {
-	var run = this.subject({timeSec : 12612}); // 3,5 hours and 12 seconds
-	run.set("timeStackSec", "20"); // 3,5 hours and 20 seconds
-	assert.strictEqual(run.get("timeStackHr"), 3);
-	assert.strictEqual(run.get("timeStackMin"), 30);
-	assert.strictEqual(run.get("timeStackSec"), 20);
-	assert.strictEqual(run.get("timeSec"), 12620);
-});
-
-// timeStackMin
-test('timeStackMin property is calculated from timeSec', function(assert) {
-	var run = this.subject({timeSec : 145});
- 	assert.strictEqual(run.get("timeStackMin"), 2);
-});
-
-test('timeStackMin setter changes timeStackMin', function(assert) {
+test('timeMin setter changes timeSec', function(assert) {
 	var run = this.subject();
-	run.set("timeStackMin", "10");
-	assert.strictEqual(run.get("timeStackMin"), 10);
-});
-
-test('timeStackMin setter can handle floats', function(assert) {
-	var run = this.subject();
-	run.set("timeStackMin", "2.2");
-	assert.strictEqual(run.get("timeStackMin"), 2);
-	run.set("timeStackMin", 2.5);
-	assert.strictEqual(run.get("timeStackMin"), 3);
-});
-
-test('timeStackMin setter also works with integer', function(assert) {
-	var run = this.subject();
-	run.set("timeStackMin", 2);
-	assert.strictEqual(run.get("timeStackMin"), 2);
-});
-
-test('timeStackMin setter influences all time related properties', function(assert) {
-	var run = this.subject({timeSec : 90}); // 1 minute, 30 seconds
-	run.set("timeStackMin", "10"); // 10 minutes, 30 seconds
-	assert.strictEqual(run.get("timeStackHr"), 0);
-	assert.strictEqual(run.get("timeStackMin"), 10);
-	assert.strictEqual(run.get("timeStackSec"), 30);
-	assert.strictEqual(run.get("timeSec"), 630);
+	run.set("timeMin", "12");
+	assert.strictEqual(run.get("timeSec"), 720);
+	run.set("timeMin", "12.123");
+	assert.strictEqual(run.get("timeSec"), 727.38);
 });
 
 // timeStackHr
@@ -204,156 +134,74 @@ test('timeStackHr setter influences all time related properties', function(asser
 	assert.strictEqual(run.get("timeSec"), 9012); 
 });
 
-// lengthMi
-test('lengthMi property is calculated from lengthM', function(assert) {
-	var run = this.subject({lengthM : 1609.344});
- 	assert.strictEqual(run.get("lengthMi"), "1.0000");
+// timeStackMin
+test('timeStackMin property is calculated from timeSec', function(assert) {
+	var run = this.subject({timeSec : 145});
+ 	assert.strictEqual(run.get("timeStackMin"), 2);
 });
 
-test('lengthMi has 4 digit precision and can round up', function(assert) {
-	var run = this.subject({lengthM : 12000});
- 	assert.strictEqual(run.get("lengthMi"), "7.4565");
-});
-
-test('lengthMi can round down', function(assert) {
-	var run = this.subject({lengthM : 11550});
- 	assert.strictEqual(run.get("lengthMi"), "7.1768");
-});
-
-test('lengthMi setter changes lengthMi', function(assert) {
+test('timeStackMin setter changes timeStackMin', function(assert) {
 	var run = this.subject();
-	run.set("lengthMi", "100");
-	assert.strictEqual(run.get("lengthMi"), "100.0000");
+	run.set("timeStackMin", "10");
+	assert.strictEqual(run.get("timeStackMin"), 10);
 });
 
-test('lengthMi setter can handle floats', function(assert) {
+test('timeStackMin setter can handle floats', function(assert) {
 	var run = this.subject();
-	run.set("lengthMi", "100.55");
-	assert.strictEqual(run.get("lengthMi"), "100.5500");
-	run.set("lengthMi", 100.12);
-	assert.strictEqual(run.get("lengthMi"), "100.1200");
+	run.set("timeStackMin", "2.2");
+	assert.strictEqual(run.get("timeStackMin"), 2);
+	run.set("timeStackMin", 2.5);
+	assert.strictEqual(run.get("timeStackMin"), 3);
 });
 
-test('lengthMi setter also works with integer', function(assert) {
+test('timeStackMin setter also works with integer', function(assert) {
 	var run = this.subject();
-	run.set("lengthMi", 100);
-	assert.strictEqual(run.get("lengthMi"), "100.0000");
+	run.set("timeStackMin", 2);
+	assert.strictEqual(run.get("timeStackMin"), 2);
 });
 
-test('lengthMi setter changes lengthM', function(assert) {
+test('timeStackMin setter influences all time related properties', function(assert) {
+	var run = this.subject({timeSec : 90}); // 1 minute, 30 seconds
+	run.set("timeStackMin", "10"); // 10 minutes, 30 seconds
+	assert.strictEqual(run.get("timeStackHr"), 0);
+	assert.strictEqual(run.get("timeStackMin"), 10);
+	assert.strictEqual(run.get("timeStackSec"), 30);
+	assert.strictEqual(run.get("timeSec"), 630);
+});
+
+// timeStackSec
+test('timeStackSec property is calculated from timeSec', function(assert) {
+	var run = this.subject({timeSec : 62});
+ 	assert.strictEqual(run.get("timeStackSec"), 2);
+});
+
+test('timeStackSec setter changes timeStackSec', function(assert) {
 	var run = this.subject();
-	run.set("lengthMi", "12");
-	assert.strictEqual(run.get("lengthM"), 19312.128);
-	run.set("lengthMi", "12.123");
-	assert.strictEqual(run.get("lengthM"), 19510.077312);
+	run.set("timeStackSec", "10");
+	assert.strictEqual(run.get("timeStackSec"), 10);
 });
 
-// lengthMiStackMi
-test('lengthMiStackMi property is calculated from lengthM', function(assert) {
-	var run = this.subject({lengthM : 1800});
- 	assert.strictEqual(run.get("lengthMiStackMi"), 1);
-});
-
-test('lengthMiStackMi property can be zero', function(assert) {
-	var run = this.subject({lengthM : 800});
- 	assert.strictEqual(run.get("lengthMiStackMi"), 0);
-});
-
-test('lengthMiStackMi setter changes lengthMiStackMi', function(assert) {
+test('timeStackSec setter can handle floats', function(assert) {
 	var run = this.subject();
-	run.set("lengthMiStackMi", "2");
-	assert.strictEqual(run.get("lengthMiStackMi"), 2);
+	run.set("timeStackSec", "2.2");
+	assert.strictEqual(run.get("timeStackSec"), 2);
+	run.set("timeStackSec", 2.5);
+	assert.strictEqual(run.get("timeStackSec"), 3);
 });
 
-test('lengthMiStackMi setter can handle floats', function(assert) {
+test('timeStackSec setter also works with integer', function(assert) {
 	var run = this.subject();
-	run.set("lengthMiStackMi", "2.2");
-	assert.strictEqual(run.get("lengthMiStackMi"), 2);
-	run.set("lengthMiStackMi", 2.5);
-	assert.strictEqual(run.get("lengthMiStackMi"), 3);
+	run.set("timeStackSec", 2);
+	assert.strictEqual(run.get("timeStackSec"), 2);
 });
 
-test('lengthMiStackMi setter also works with integer', function(assert) {
-	var run = this.subject();
-	run.set("lengthMiStackMi", 2);
-	assert.strictEqual(run.get("lengthMiStackMi"), 2);
-});
-
-test('lengthMiStackMi setter changes lengthM', function(assert) {
-	var run = this.subject({lengthM : 1234});
-	run.set("lengthMiStackMi", "2");
-	assert.strictEqual(run.get("lengthM"), 1234+3218.688);
-});
-
-// lengthMiStackDecimal
-test('lengthMiStackDecimal property is calculated from lengthM and can round down', function(assert) {
-	var run = this.subject({lengthM : 2711});
- 	assert.strictEqual(run.get("lengthMiStackDecimal"), "68");
-});
-
-test('lengthMiStackDecimal can round up', function(assert) {
-	var run = this.subject({lengthM : 2712});
- 	assert.strictEqual(run.get("lengthMiStackDecimal"), "69");
-});
-
-test('lengthMiStackDecimal can have 1 digit', function(assert) {
-	var run = this.subject({lengthM : 804.672});
- 	assert.strictEqual(run.get("lengthMiStackDecimal"), "5");
-});
-
-test('lengthMiStackDecimal supports leading zero', function(assert) {
-	var run = this.subject({lengthM : 100});
- 	assert.strictEqual(run.get("lengthMiStackDecimal"), "06");
-});
-
-test('lengthMiStackDecimal can be zero', function(assert) {
-	var run = this.subject({lengthM : 1609.344});
- 	assert.strictEqual(run.get("lengthMiStackDecimal"), "0");
-});
-
-test('lengthMiStackDecimal setter changes lengthMiStackDecimal', function(assert) {
-	var run = this.subject();
-	run.set("lengthMiStackDecimal", "9");
-	assert.strictEqual(run.get("lengthMiStackDecimal"), "9");
-});
-
-test('lengthMiStackDecimal setter can handle floats', function(assert) {
-	var run = this.subject();
-	run.set("lengthMiStackDecimal", 9.2);
-	assert.strictEqual(run.get("lengthMiStackDecimal"), "9");
-	run.set("lengthMiStackDecimal", "9.5");
-	assert.strictEqual(run.get("lengthMiStackDecimal"), "1");
-});
-
-test('lengthMiStackDecimal setter also works with integer', function(assert) {
-	var run = this.subject();
-	run.set("lengthMiStackDecimal", 9);
-	assert.strictEqual(run.get("lengthMiStackDecimal"), "9");
-});
-
-test('lengthMiStackDecimal setter works with leading zeros', function(assert) {
-	var run = this.subject();
-	run.set("lengthMiStackDecimal", "09");
-	assert.strictEqual(run.get("lengthMiStackDecimal"), "09");
-	run.set("lengthMiStackDecimal", "002");
-	assert.strictEqual(run.get("lengthMiStackDecimal"), "0");
-	run.set("lengthMiStackDecimal", "009");
-	assert.strictEqual(run.get("lengthMiStackDecimal"), "01");
-});
-
-test('lengthMiStackDecimal setter changes lengthM', function(assert) {
-	var run = this.subject({lengthM : 2000}); // around 1.24 miles
-	run.set("lengthMiStackDecimal", "09");
-	assert.strictEqual(run.get("lengthM"), 1754.18496); // 1.09 miles
-});
-
-test('lengthMiStackDecimal and lengthMiStackDecimal setter will define lengthMi', function(assert) {
-	var run = this.subject();
-	run.setProperties({
-		"lengthMiStackMi" : "12",
-		"lengthMiStackDecimal" : "09"
-	});
-	assert.strictEqual(run.get("lengthMi"), "12.0900");
+test('timeStackSec setter influences all time related properties', function(assert) {
+	var run = this.subject({timeSec : 12612}); // 3,5 hours and 12 seconds
+	run.set("timeStackSec", "20"); // 3,5 hours and 20 seconds
+	assert.strictEqual(run.get("timeStackHr"), 3);
+	assert.strictEqual(run.get("timeStackMin"), 30);
+	assert.strictEqual(run.get("timeStackSec"), 20);
+	assert.strictEqual(run.get("timeSec"), 12620);
 });
 
 // lengthKm
@@ -506,6 +354,158 @@ test('lengthKmStackKm and lengthKmStackDecimal setter will define lengthKm', fun
 		"lengthKmStackDecimal" : "09"
 	});
 	assert.strictEqual(run.get("lengthKm"), "12.0900");
+});
+
+// lengthMi
+test('lengthMi property is calculated from lengthM', function(assert) {
+	var run = this.subject({lengthM : 1609.344});
+ 	assert.strictEqual(run.get("lengthMi"), "1.0000");
+});
+
+test('lengthMi has 4 digit precision and can round up', function(assert) {
+	var run = this.subject({lengthM : 12000});
+ 	assert.strictEqual(run.get("lengthMi"), "7.4565");
+});
+
+test('lengthMi can round down', function(assert) {
+	var run = this.subject({lengthM : 11550});
+ 	assert.strictEqual(run.get("lengthMi"), "7.1768");
+});
+
+test('lengthMi setter changes lengthMi', function(assert) {
+	var run = this.subject();
+	run.set("lengthMi", "100");
+	assert.strictEqual(run.get("lengthMi"), "100.0000");
+});
+
+test('lengthMi setter can handle floats', function(assert) {
+	var run = this.subject();
+	run.set("lengthMi", "100.55");
+	assert.strictEqual(run.get("lengthMi"), "100.5500");
+	run.set("lengthMi", 100.12);
+	assert.strictEqual(run.get("lengthMi"), "100.1200");
+});
+
+test('lengthMi setter also works with integer', function(assert) {
+	var run = this.subject();
+	run.set("lengthMi", 100);
+	assert.strictEqual(run.get("lengthMi"), "100.0000");
+});
+
+test('lengthMi setter changes lengthM', function(assert) {
+	var run = this.subject();
+	run.set("lengthMi", "12");
+	assert.strictEqual(run.get("lengthM"), 19312.128);
+	run.set("lengthMi", "12.123");
+	assert.strictEqual(run.get("lengthM"), 19510.077312);
+});
+
+// lengthMiStackMi
+test('lengthMiStackMi property is calculated from lengthM', function(assert) {
+	var run = this.subject({lengthM : 1800});
+ 	assert.strictEqual(run.get("lengthMiStackMi"), 1);
+});
+
+test('lengthMiStackMi property can be zero', function(assert) {
+	var run = this.subject({lengthM : 800});
+ 	assert.strictEqual(run.get("lengthMiStackMi"), 0);
+});
+
+test('lengthMiStackMi setter changes lengthMiStackMi', function(assert) {
+	var run = this.subject();
+	run.set("lengthMiStackMi", "2");
+	assert.strictEqual(run.get("lengthMiStackMi"), 2);
+});
+
+test('lengthMiStackMi setter can handle floats', function(assert) {
+	var run = this.subject();
+	run.set("lengthMiStackMi", "2.2");
+	assert.strictEqual(run.get("lengthMiStackMi"), 2);
+	run.set("lengthMiStackMi", 2.5);
+	assert.strictEqual(run.get("lengthMiStackMi"), 3);
+});
+
+test('lengthMiStackMi setter also works with integer', function(assert) {
+	var run = this.subject();
+	run.set("lengthMiStackMi", 2);
+	assert.strictEqual(run.get("lengthMiStackMi"), 2);
+});
+
+test('lengthMiStackMi setter changes lengthM', function(assert) {
+	var run = this.subject({lengthM : 1234});
+	run.set("lengthMiStackMi", "2");
+	assert.strictEqual(run.get("lengthM"), 1234+3218.688);
+});
+
+// lengthMiStackDecimal
+test('lengthMiStackDecimal property is calculated from lengthM and can round down', function(assert) {
+	var run = this.subject({lengthM : 2711});
+ 	assert.strictEqual(run.get("lengthMiStackDecimal"), "68");
+});
+
+test('lengthMiStackDecimal can round up', function(assert) {
+	var run = this.subject({lengthM : 2712});
+ 	assert.strictEqual(run.get("lengthMiStackDecimal"), "69");
+});
+
+test('lengthMiStackDecimal can have 1 digit', function(assert) {
+	var run = this.subject({lengthM : 804.672});
+ 	assert.strictEqual(run.get("lengthMiStackDecimal"), "5");
+});
+
+test('lengthMiStackDecimal supports leading zero', function(assert) {
+	var run = this.subject({lengthM : 100});
+ 	assert.strictEqual(run.get("lengthMiStackDecimal"), "06");
+});
+
+test('lengthMiStackDecimal can be zero', function(assert) {
+	var run = this.subject({lengthM : 1609.344});
+ 	assert.strictEqual(run.get("lengthMiStackDecimal"), "0");
+});
+
+test('lengthMiStackDecimal setter changes lengthMiStackDecimal', function(assert) {
+	var run = this.subject();
+	run.set("lengthMiStackDecimal", "9");
+	assert.strictEqual(run.get("lengthMiStackDecimal"), "9");
+});
+
+test('lengthMiStackDecimal setter can handle floats', function(assert) {
+	var run = this.subject();
+	run.set("lengthMiStackDecimal", 9.2);
+	assert.strictEqual(run.get("lengthMiStackDecimal"), "9");
+	run.set("lengthMiStackDecimal", "9.5");
+	assert.strictEqual(run.get("lengthMiStackDecimal"), "1");
+});
+
+test('lengthMiStackDecimal setter also works with integer', function(assert) {
+	var run = this.subject();
+	run.set("lengthMiStackDecimal", 9);
+	assert.strictEqual(run.get("lengthMiStackDecimal"), "9");
+});
+
+test('lengthMiStackDecimal setter works with leading zeros', function(assert) {
+	var run = this.subject();
+	run.set("lengthMiStackDecimal", "09");
+	assert.strictEqual(run.get("lengthMiStackDecimal"), "09");
+	run.set("lengthMiStackDecimal", "002");
+	assert.strictEqual(run.get("lengthMiStackDecimal"), "0");
+	run.set("lengthMiStackDecimal", "009");
+	assert.strictEqual(run.get("lengthMiStackDecimal"), "01");
+});
+
+test('lengthMiStackDecimal setter changes lengthM', function(assert) {
+	var run = this.subject({lengthM : 2000}); // around 1.24 miles
+	run.set("lengthMiStackDecimal", "09");
+	assert.strictEqual(run.get("lengthM"), 1754.18496); // 1.09 miles
+});
+
+test('lengthMiStackDecimal and lengthMiStackDecimal setter will define lengthMi', function(assert) {
+	var run = this.subject();
+	run.setProperties({
+		"lengthMiStackMi" : "12",
+		"lengthMiStackDecimal" : "09"
+	});
+	assert.strictEqual(run.get("lengthMi"), "12.0900");
 });
 
 // speedKmHr
