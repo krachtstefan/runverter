8443676585f28c7531613ecab8331b7de2e3ff4f
Stefan Kracht <stefankracht@gmail.com>
2015-07-28 11:32:37 +0200

    add SelectOrDie assets

--- Brocfile.js
+++ Brocfile.js
@@ -6,4 +6,6 @@ var app = new EmberApp();
 app.import("bower_components/ember-localstorage-adapter/localstorage_adapter.js");
 app.import('bower_components/uikit/css/uikit.min.css');
 app.import('bower_components/uikit/js/uikit.min.js');
+app.import('bower_components/SelectOrDie/_src/selectordie.min.js');
+app.import('bower_components/SelectOrDie/_src/selectordie.css');
 module.exports = app.toTree();
