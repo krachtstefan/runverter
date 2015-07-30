f2d0262ee1798ba64a3e683bba1876c353a8617c
Stefan Kracht <stefankracht@gmail.com>
2015-07-30 10:38:11 +0200

    reflect run length options via select field

--- app/views/pace_calculator.js
+++ app/views/pace_calculator.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import $ from 'jquery';
 export default Ember.View.extend({
+	contentBinding : "controller",
   didInsertElement: function() {
     $(".selectordie").selectOrDie();
   }
