135ce459355ad2e136a95bde2d4d81d9a6549b94
Stefan Kracht <stefankracht@gmail.com>
2015-07-30 11:56:23 +0200

    handle selected tempo in pace calculator view

--- app/views/pace_calculator.js
+++ app/views/pace_calculator.js
@@ -11,6 +11,24 @@ export default Ember.View.extend({
     return this.get("content.runLengthSelected") === "mi" ? true : false;
   }.property('content.runLengthSelected'),
 
+  showRunTempoMinKm: function () {
+    return this.get("content.runTempoSelected") === "minkm" ? true : false;
+  }.property('content.runTempoSelected'),
+
+  showRunTempoMinMi: function () {
+    return this.get("content.runTempoSelected") === "minmi" ? true : false;
+  }.property('content.runTempoSelected'),
+
+  showRunTempoKmH: function () {
+    return this.get("content.runTempoSelected") === "kmh" ? true : false;
+  }.property('content.runTempoSelected'),
+
+  showRunTempoMiH: function () {
+    return this.get("content.runTempoSelected") === "mih" ? true : false;
+  }.property('content.runTempoSelected'),
+
+  
+
   didInsertElement: function() {
     $(".selectordie").selectOrDie();
   }
