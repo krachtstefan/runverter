dab9798f0ccc66cefb4e2eed67296747084902bf
Stefan Kracht <stefankracht@gmail.com>
2015-07-30 10:56:56 +0200

    add computed view propteries to check which run length metrics should be visible

--- app/views/pace_calculator.js
+++ app/views/pace_calculator.js
@@ -2,6 +2,15 @@ import Ember from 'ember';
 import $ from 'jquery';
 export default Ember.View.extend({
 	contentBinding : "controller",
+	
+	showRunLengthKm: function () {
+    return this.get("content.runLengthSelected") === "km" ? true : false
+  }.property('content.runLengthSelected'),
+
+  showRunLengthMi: function () {
+    return this.get("content.runLengthSelected") === "mi" ? true : false
+  }.property('content.runLengthSelected'),
+
   didInsertElement: function() {
     $(".selectordie").selectOrDie();
   }
