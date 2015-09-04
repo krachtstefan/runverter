204615a12b866ef2799dda33da0d996425e578c5
Stefan Kracht <stefankracht@gmail.com>
2015-09-04 12:53:20 +0200

    run ember init and merge changes

--- app/router.js
+++ app/router.js
@@ -5,6 +5,9 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-export default Router.map(function() {
+Router.map(function() {
 	this.resource('paceCalculator', { path: '/' }, function(){});
 });
+
+
+export default Router;
