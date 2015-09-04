4e42be3b53a1193916aacff962308252cbd2670d
Stefan Kracht <stefankracht@gmail.com>
2015-09-04 10:09:38 +0200

    add initial deploy configuration for development environment

---
+++ config/deploy.js
@@ -0,0 +1,20 @@
+module.exports = {
+  development: {
+    buildEnv: 'development',
+    store: {
+      type: 'redis',
+      host: 'localhost',
+      port: 6379
+    },
+    assets: {
+      type: 's3',
+      gzip: false,
+      gzipExtensions: ['js', 'css', 'svg'], // if undefined, js, css & svg files are gziped
+      exclude: ['.DS_Store', '*-test.js'],
+      accessKeyId: 'AKIAJQH4RE42AHIISVXQ',
+      secretAccessKey: process.env['AWS_ACCESS_KEY'],
+      bucket: 'runverter-development',  
+      region: "eu-west-1"
+    }
+  }
+};
