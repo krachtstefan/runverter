a1af46a07035e32d02b730cbe359e235cf3eb138
Stefan Kracht <stefankracht@gmail.com>
2015-09-07 12:11:28 +0200

    move variables from view to controller to get rid of view namespace in template

--- app/controllers/pace_calculator.js
+++ app/controllers/pace_calculator.js
@@ -31,5 +31,29 @@ export default Ember.Controller.extend({
 			id : "mih",
 			label : "mi/h"
 		}
-	]
+	],
+
+	showRunLengthKm: function () {
+    return this.get("runLengthSelected") === "km" ? true : false;
+  }.property('runLengthSelected'),
+
+  showRunLengthMi: function () {
+    return this.get("runLengthSelected") === "mi" ? true : false;
+  }.property('runLengthSelected'),
+
+  showRunTempoMinKm: function () {
+    return this.get("runTempoSelected") === "minkm" ? true : false;
+  }.property('runTempoSelected'),
+
+  showRunTempoMinMi: function () {
+    return this.get("runTempoSelected") === "minmi" ? true : false;
+  }.property('runTempoSelected'),
+
+  showRunTempoKmH: function () {
+    return this.get("runTempoSelected") === "kmh" ? true : false;
+  }.property('runTempoSelected'),
+
+  showRunTempoMiH: function () {
+    return this.get("runTempoSelected") === "mih" ? true : false;
+  }.property('runTempoSelected'),
 });
--- app/views/pace_calculator.js
+++ app/views/pace_calculator.js
@@ -3,32 +3,6 @@ import $ from 'jquery';
 export default Ember.View.extend({
 	contentBinding : "controller",
 	
-	showRunLengthKm: function () {
-    return this.get("content.runLengthSelected") === "km" ? true : false;
-  }.property('content.runLengthSelected'),
-
-  showRunLengthMi: function () {
-    return this.get("content.runLengthSelected") === "mi" ? true : false;
-  }.property('content.runLengthSelected'),
-
-  showRunTempoMinKm: function () {
-    return this.get("content.runTempoSelected") === "minkm" ? true : false;
-  }.property('content.runTempoSelected'),
-
-  showRunTempoMinMi: function () {
-    return this.get("content.runTempoSelected") === "minmi" ? true : false;
-  }.property('content.runTempoSelected'),
-
-  showRunTempoKmH: function () {
-    return this.get("content.runTempoSelected") === "kmh" ? true : false;
-  }.property('content.runTempoSelected'),
-
-  showRunTempoMiH: function () {
-    return this.get("content.runTempoSelected") === "mih" ? true : false;
-  }.property('content.runTempoSelected'),
-
-  
-
   didInsertElement: function() {
     $("select.runLength").selectOrDie({customID:"runLength"});
     $("select.runTempo").selectOrDie({customID:"runTempo"});
