655d8b36de68dec19fbb1f806565201f7683cb11
Stefan Kracht <stefankracht@gmail.com>
2015-09-09 10:17:56 +0200

    setup production like builds for staging and production environment

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,26 +1,24 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var env = EmberApp.env();
+var isProductionLikeBuild = ['production', 'staging'].indexOf(env) > -1;
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      enabled: isProductionLikeBuild,
+      prepend: 'http://runverter-development.s3-website-eu-west-1.amazonaws.com/'
+    },
+    sourcemaps: {
+      enabled: !isProductionLikeBuild,
+    },
+    minifyCSS: { enabled: isProductionLikeBuild },
+    minifyJS: { enabled: isProductionLikeBuild },
+
+    tests: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
+    hinting: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-  // 
-  // 
-  
   app.import("bower_components/ember-localstorage-adapter/localstorage_adapter.js");
   app.import('bower_components/uikit/css/uikit.min.css');
   app.import('bower_components/uikit/js/uikit.min.js');
