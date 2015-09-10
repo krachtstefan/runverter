2bd6b22b919955936efccc7939b75b6a3c839618
Stefan Kracht <stefankracht@gmail.com>
2015-09-10 15:20:31 +0200

    add production environment to deploy.js

--- config/deploy.js
+++ config/deploy.js
@@ -11,7 +11,23 @@ module.exports = {
       exclude: ['.DS_Store', '*-test.js'],
       accessKeyId: 'AKIAJQH4RE42AHIISVXQ',
       secretAccessKey: process.env['AWS_ACCESS_KEY'],
-      bucket: 'runverter-development',  
+      bucket: 'runverter-development',
+      region: "eu-west-1"
+    }
+  },
+  production: {
+    buildEnv: 'production',
+    store: {
+      type: 'redis',
+      host: 'localhost',
+      port: 6380
+    },
+    assets: {
+      type: 's3',
+      exclude: ['.DS_Store', '*-test.js'],
+      accessKeyId: 'AKIAJQH4RE42AHIISVXQ',
+      secretAccessKey: process.env['AWS_ACCESS_KEY'],
+      bucket: 'runverter-development',
       region: "eu-west-1"
     }
   }
