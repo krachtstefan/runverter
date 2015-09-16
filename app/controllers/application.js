f650fca44af45b88c7c3e813220bbf83c22e3ab3
Stefan Kracht <stefankracht@gmail.com>
2015-09-16 10:50:04 +0200

    add menuItems array to application controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,2 +1,35 @@
 import Ember from 'ember';
-export default Ember.Controller.extend({});
+export default Ember.Controller.extend({
+  menuItems : [
+    {
+      route : "paceCalculator",
+      name : "Pace Calculator",
+      label : "You're using the",
+      description : "See which pace you need, to finish a run in your desired time."
+    },
+    {
+      route : "paceConverter",
+      name : "Pace Converter",
+      label : "You're using the",
+      description : "Some description."
+    },
+    {
+      route : "lengthConverter",
+      name : "Lenght Converter",
+      label : "km",
+      description : "Some description."
+    },
+    {
+      route : "racePredictor",
+      name : "Race Predictor",
+      label : "You're using the",
+      description : "Some description."
+    },
+    {
+      route : "splitsCalculator",
+      name : "Split Time Calculator",
+      label : "You're using the",
+      description : "Some description."
+    }
+  ],
+});
