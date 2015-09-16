e108502b9de8e92f15e74383023b583ad75615c5
Stefan Kracht <stefankracht@gmail.com>
2015-09-16 12:36:24 +0200

    Accidently commited debug code. Stupid me!

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -34,7 +34,7 @@ export default Ember.Controller.extend({
   ],
 
   menuItemsSelected : function() {
-    var currentPath = this.get("currentPath").split(".")[1];
+    var currentPath = this.get("currentPath").split(".")[0];
     var foundMenuItem = this.get("menuItems").findBy("route", currentPath);
     return foundMenuItem ? foundMenuItem : this.get("menuItems")[0];
   }.property('currentPath'),
