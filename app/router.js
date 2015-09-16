08c88407e5cecdf18437302ba3431be0fd8ed49a
Stefan Kracht <stefankracht@gmail.com>
2015-09-16 09:50:58 +0200

    create pace converter

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
 	this.resource('paceCalculator', { path: '/' }, function(){});
+  this.resource('paceConverter', { path: '/pace' }, function(){});
 });
 
 
