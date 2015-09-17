88192ec4d2f56ca41454f357613d23bbfbeb889f
Stefan Kracht <stefankracht@gmail.com>
2015-09-17 11:58:52 +0200

    add model to pace converter

--- app/routes/pace_converter.js
+++ app/routes/pace_converter.js
@@ -1,2 +1,9 @@
 import Ember from 'ember';
-export default Ember.Route.extend();
+export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('run', {
+      timeSec : 3600*4,
+      lengthM : 42195,
+    });
+  }
+});
