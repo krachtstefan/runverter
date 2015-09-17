602fa32e84b0553457ff1c73141873be35a7c368
Stefan Kracht <stefankracht@gmail.com>
2015-09-17 11:29:32 +0200

    fix initial selectOrDie menue selection
    did not work properly when the selected value was not the first one

--- app/views/application.js
+++ app/views/application.js
@@ -3,7 +3,9 @@ import $ from 'jquery';
 export default Ember.View.extend({
   didInsertElement: function() {
     Ember.run.scheduleOnce('afterRender', this, function() {
-      $("select.menu").selectOrDie({customID:"menu"});
+      $("select.menu").selectOrDie({customID:"menu"}).ready(function() {
+        $(".menu").selectOrDie("update"); // need to trigger update to select the correct initial value
+      });
     });
   }
 });
