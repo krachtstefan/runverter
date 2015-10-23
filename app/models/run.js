2061516ce8397dabfbf5ee3ab1aa8665ea9f71d8
Stefan Kracht <stefankracht@gmail.com>
2015-10-23 11:07:06 +0200

    use BigNumber for mToMi

--- app/models/run.js
+++ app/models/run.js
@@ -4,9 +4,9 @@ export default DS.Model.extend({
 	/**
 	 * MtoMi the lenght of a meter in miles
 	 *
-	 * @type {number}	lenght of a meter in miles
+	 * @type {BigNumber}	lenght of a meter in miles
 	 */
-	mToMi : 0.000621371,
+	mToMi : new BigNumber(0.000621371),
 
 	/**
 	 * MitoM the lenght of a mile in meters
