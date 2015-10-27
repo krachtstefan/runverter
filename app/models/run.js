54053b1cc6db257658fe73c7dcd735351c3d4479
Stefan Kracht <stefankracht@gmail.com>
2015-10-27 14:58:57 +0100

    implement BigNumber to paceMinPerMiStackMin

--- app/models/run.js
+++ app/models/run.js
@@ -302,15 +302,15 @@ export default DS.Model.extend({
 	 *
 	 * @param  {string}								propertyName		if defined, it will be paceMinPerMiStackMin
 	 * @param  {Object|string|number} value						new value of paceMinPerMiStackMin
-	 * @return {number} 															min stack of the pace
+	 * @return {BigNumber} 													  min stack of the pace
 	 */
 	paceMinPerMiStackMin : function(propertyName, value) {
 		if (arguments.length > 1) {
     	var previousValue = this.get("paceMinPerMiStackMin");
-    	value = +Math.round(value) || 0; // convert to number or set to 0
-			this.set("paceMinPerMi", parseFloat(this.get('paceMinPerMi'))+(value-previousValue));
+    	value = this._ensureBigNumber(value).round();
+      this.set("paceMinPerMi", this.get('paceMinPerMi').plus(value.minus(previousValue)));
 		}
-		return parseInt(this.get("paceMinPerMi"));
+		return this.get("paceMinPerMi").floor();
 	}.property('paceMinPerMi'),
 
 	/**
