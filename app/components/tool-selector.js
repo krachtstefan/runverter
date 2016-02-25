86b67e8fc439e06d0f7221aa46dce029aae93584
Stefan Kracht <stefankracht@gmail.com>
2016-02-25 16:04:50 +0100

    remove selectOrDie from application view to component

--- app/components/tool-selector.js
+++ app/components/tool-selector.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 export default Ember.Component.extend({
+  didInsertElement: function() {
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      $("select.menu").selectOrDie({customID:"menu"}).ready(function() {
+        $(".menu").selectOrDie("update"); // need to trigger update to select the correct initial value
+      });
+    });
+  },
   actions: {
     switchTool: function(toolKey) {
       this.sendAction('action', toolKey);
