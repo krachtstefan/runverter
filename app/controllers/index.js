1e0d9aedd8af14d98f1227bc6641d112a4f3625d
Stefan Kracht <stefankracht@gmail.com>
2016-02-25 15:12:06 +0100

    rename menu into tools and clean up index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,55 +1,48 @@
 import Ember from 'ember';
 export default Ember.Controller.extend({
-  menuItems : [
+  tools : [
     {
-      route : "paceCalculator",
+      key : "paceCalculator",
       name : "Pace Calculator",
       label : "You're using the",
       description : "See which pace you need, to finish a run in your desired time."
     },
     {
-      route : "paceConverter",
+      key : "paceConverter",
       name : "Pace Converter",
       label : "Hi! I'm the",
       description : "There a lots of ways to define a tempo. Here you have all in one place."
     },
     {
-      route : "lengthConverter",
+      key : "lengthConverter",
       name : "Lenght Converter",
       label : "This is the",
       description : "Here you can see the relation of all major length units. And yes, length matters."
     },
     {
-      route : "racePredictor",
+      key : "racePredictor",
       name : "Race Predictor",
       label : "This space is reserved for the",
       description : "Not quite ready yet..."
     },
     {
-      route : "splitsCalculator",
+      key : "splitsCalculator",
       name : "Split Time Calculator",
       label : "Unfortunately this is not the",
       description : "This feature will come pretty soon. I promise."
     }
   ],
 
-  menuItemsSelected : function() {
-    return this.get("currentPath").split(".")[0];
-  }.property('currentPath'),
+  selectedToolKey : "paceCalculator",
 
-  menuDescription : function() {
-    var selectedMenuItem = this.get("menuItems").findBy("route", this.get("menuItemsSelected"));
-    return selectedMenuItem.description;
-  }.property('menuItemsSelected'),
-
-  menuLabel: function() {
-    var selectedMenuItem = this.get("menuItems").findBy("route", this.get("menuItemsSelected"));
-    return selectedMenuItem.label;
-  }.property('menuItemsSelected'),
+  selectedTool : Ember.computed("selectedToolKey", function(){
+    var selectedTool = this.get("tools").findBy("key", this.get("selectedToolKey"));
+    return selectedTool;
+  }),
 
   actions: {
     navigateTo: function(selection) {
-      this.transitionToRoute(selection);
+      this.set("selectedToolKey", selection.toString());
     }
   }
 });
