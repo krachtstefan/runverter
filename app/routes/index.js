aad2c85e031ac605dfa70322907811f51e658ead
Stefan Kracht <stefankracht@gmail.com>
2016-02-25 10:10:03 +0100

    create index route

---
+++ app/routes/index.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('run', {
+      timeSec : new BigNumber(3600*4),
+      lengthM : new BigNumber(42195)
+    });
+  }
+});
