c85d27141cf2b026360dc7982486ded9de438530
Stefan Kracht <stefankracht@gmail.com>
2016-02-26 12:16:29 +0100

    import jquery in components

--- app/components/pace-calculator.js
+++ app/components/pace-calculator.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import $ from 'jquery';
 export default Ember.Component.extend({
   queryParams: ['runLengthMetricsSelected', 'runTempoMetricsSelected'],
   runLengthMetricsSelected : "km",
