0b8dc5c3d182d1f597edfa4061c82100552577de
Stefan Kracht <stefankracht@gmail.com>
2016-03-01 12:38:33 +0100

    make keys and queryParams short to prevent long urls

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,43 +1,43 @@
 import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: {
-    selectedToolKey : 'tool'
+    selectedToolKey : 't'
   },
 
   tools : [
     {
-      key : "paceCalculator",
+      key : "pca",
       name : "Pace Calculator",
       label : "You're using the",
       description : "See which pace you need, to finish a run in your desired time."
     },
     {
-      key : "paceConverter",
+      key : "pc",
       name : "Pace Converter",
       label : "Hi! I'm the",
       description : "There a lots of ways to define a tempo. Here you have all in one place."
     },
     {
-      key : "lengthConverter",
+      key : "lc",
       name : "Lenght Converter",
       label : "This is the",
       description : "Here you can see the relation of all major length units. And yes, length matters."
     },
     {
-      key : "racePredictor",
+      key : "rp",
       name : "Race Predictor",
       label : "This space is reserved for the",
       description : "Not quite ready yet..."
     },
     {
-      key : "splitsCalculator",
+      key : "sc",
       name : "Split Time Calculator",
       label : "Unfortunately this is not the",
       description : "This feature will come pretty soon. I promise."
     }
   ],
 
-  selectedToolKey : "paceCalculator",
+  selectedToolKey : "pca",
 
   selectedTool : Ember.computed("selectedToolKey", function(){
     var selectedTool = this.get("tools").findBy("key", this.get("selectedToolKey"));
