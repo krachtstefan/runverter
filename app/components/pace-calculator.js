fa55d7a9b3e38809ef5e8ee387989dc3367251d3
Stefan Kracht <stefankracht@gmail.com>
2016-03-02 11:55:45 +0100

    remove obsolete queryParams property in pace calculator component

--- app/components/pace-calculator.js
+++ app/components/pace-calculator.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import $ from 'jquery';
 export default Ember.Component.extend({
-  queryParams: ['runLengthMetricsSelected', 'runTempoMetricsSelected'],
 
   runLengthMetricsSelected : "km",
 
