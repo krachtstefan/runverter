c1b54e99667533e0fd1ec5a428867d517810a33b
Stefan Kracht <stefankracht@gmail.com>
2016-03-02 16:27:36 +0100

    customize serializer to store timeSec in timeSecStorage and lengthM in lengthMStorage

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,2 +1,10 @@
 import { LSSerializer } from 'ember-localstorage-adapter';
-export default LSSerializer.extend();
+export default LSSerializer.extend({
+
+  serialize(snapshot) {
+    var json = this._super(...arguments);
+    json.timeSecStorage = snapshot.record.get("timeSec").toString();
+    json.lengthMStorage = snapshot.record.get("lengthM").toString();
+    return json;
+  }
+});
