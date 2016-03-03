5a1e0159c17a1539437ece8f7812c86a3b5ab723
Stefan Kracht <stefankracht@gmail.com>
2016-03-03 16:21:48 +0100

    Revert "customize serializer to store timeSec in timeSecStorage and lengthM in lengthMStorage"
    This reverts commit dc7c30dd54b5535622d937611c3dc3e4e74f773f.

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,10 +1,2 @@
 import { LSSerializer } from 'ember-localstorage-adapter';
-export default LSSerializer.extend({
-
-  serialize(snapshot) {
-    var json = this._super(...arguments);
-    json.timeSecStorage = snapshot.record.get("timeSec").toString();
-    json.lengthMStorage = snapshot.record.get("lengthM").toString();
-    return json;
-  }
-});
+export default LSSerializer.extend();
