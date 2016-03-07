8d8486f5f2933c81f35445719d43b134039f19bd
Stefan Kracht <stefankracht@gmail.com>
2016-03-07 16:38:41 +0100

    add tools to english language file

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -1,11 +1,29 @@
 export default {
-  // "some.translation.key": "Text for some.translation.key",
-  //
-  // "a": {
-  //   "nested": {
-  //     "key": "Text for a.nested.key"
-  //   }
-  // },
-  //
-  // "key.with.interpolation": "Text with {{anInterpolation}}"
+  "tools" : {
+    "pca" : {
+      "name" : "Pace Calculator",
+      "label" : "You're using the",
+      "description" : "See which pace you need, to finish a run in your desired time."
+    },
+    "pc" : {
+      "name" : "Pace Converter",
+      "label" : "Hi! I'm the",
+      "description" : "There a lots of ways to define a tempo. Here you have all in one place."
+    },
+    "lc" : {
+      "name" : "Lenght Converter",
+      "label" : "This is the",
+      "description" : "Here you can see the relation of all common length units. And yes, length matters."
+    },
+    "rp" : {
+      "name" : "Race Predictor",
+      "label" : "This space is reserved for the",
+      "description" : "Not quite ready yet..."
+    },
+    "sc" : {
+      "name" : "Split Time Calculator",
+      "label" : "Unfortunately this is not the",
+      "description" : "This feature will come pretty soon. I promise."
+    }
+  }
 };
