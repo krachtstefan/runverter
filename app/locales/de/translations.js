dcf12e401c6a6e39b685a1526733b78e4a1fc29f
Stefan Kracht <stefankracht@gmail.com>
2016-03-09 17:13:54 +0100

    initial german translation

--- app/locales/de/translations.js
+++ app/locales/de/translations.js
@@ -1,11 +1,60 @@
 export default {
-  // "some.translation.key": "Text for some.translation.key",
-  //
-  // "a": {
-  //   "nested": {
-  //     "key": "Text for a.nested.key"
-  //   }
-  // },
-  //
-  // "key.with.interpolation": "Text with {{anInterpolation}}"
+  "tools" : {
+    "pca" : {
+      "name" : "Tempo Rechner",
+      "label" : "Du benutzt den",
+      "description" : "Finde heraus wie schnell du sein musst, um eine Strecke in gewünschter Zeit zu schaffen.",
+      "distanceLabel" : "Für eine Strecke von",
+      "timeLabel" : "in",
+      "paceLabel" : "benötigst du eine Geschwindigkeit von"
+    },
+    "pc" : {
+      "name" : "Tempo Umrechner",
+      "label" : "Hi! Ich bin der",
+      "description" : "Es gibt viele Arten deine Geschwindigkeit anzugeben. Hier hast du alle im Blick.",
+      "minkmLabel" : "Eine Pace von",
+      "minmiLabel" : "oder",
+      "kmhLabel" : "Entspricht einer Geschwindigkeit von",
+      "mihLabel" : "bzw."
+    },
+    "lc" : {
+      "name" : "Strecken Umrechner",
+      "label" : "Das ist der",
+      "description" : "Hier findest du die wichtigsten Längenangaben. Und ja, die Länge ist entscheidend.",
+      "kmLabel" : "Eine Strecke von",
+      "miLabel" : "entspricht",
+      "mLabel" : "bzw."
+    },
+    "rp" : {
+      "name" : "Wettkampfprognose",
+      "label" : "Dieser Platze ist reserviert für die",
+      "description" : "Noch nicht ganz fertig..."
+    },
+    "sc" : {
+      "name" : "Zwischenzeit Rechner",
+      "label" : "Das ist nicht der",
+      "description" : "Aber hier wird es ihn bald geben. Versprochen."
+    }
+  },
+  "metrics" : {
+    "distance" : {
+      "separator" : ",",
+      "km" : "km",
+      "mi" : "mi",
+      "m" : "m"
+    },
+    "tempo" : {
+      "separator" : ":",
+      "minkm" : "min/km",
+      "minmi" : "min/mi",
+      "kmh" : "km/h",
+      "mih" : "mi/h",
+    },
+    "time" : {
+      "separator" : ":",
+      "hr" : "Std",
+      "min" : "Min",
+      "sec" : "Sek",
+    }
+  }
 };
