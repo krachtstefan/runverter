86334bf8f10ea290ab5b0d4a7b8e251e5b358348
Stefan Kracht <stefankracht@gmail.com>
2016-07-07 11:47:17 +0200

    switch back to public/images folder
    Some images need to be public (like favicon) anyway, so it's better to keep all images in one place

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,7 +19,7 @@ module.exports = function(defaults) {
     tests: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
     hinting: process.env.EMBER_CLI_TEST_COMMAND || !isProductionLikeBuild,
 
-    compassOptions: { imagesDir: "assets/images" }
+    compassOptions: { imagesDir: "public/images" }
   });
 
   app.import("bower_components/ember-localstorage-adapter/localstorage_adapter.js");
