7793767b83a2e496720a34c283f96061a0e9694f
Stefan Kracht <stefankracht@gmail.com>
2016-07-08 14:55:02 +0200

    fix issue where selectOrDie didn't updated after language change

--- app/components/tool-selector.js
+++ app/components/tool-selector.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 import $ from 'jquery';
 export default Ember.Component.extend({
+
+  i18n: Ember.inject.service(),
+
   didInsertElement: function() {
     Ember.run.scheduleOnce('afterRender', this, function() {
       $("select.menu").selectOrDie({customID:"menu"}).ready(function() {
@@ -8,6 +11,18 @@ export default Ember.Component.extend({
       });
     });
   },
+
+  didInitAttrs() {
+    // if not accessed once, i18n changes are not recognized by computed properties or observers
+    this.get('i18n');
+  },
+
+  updateSelectOrDieOnLanguageChange: function () {
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      $(".menu").selectOrDie("update"); // need to trigger update to select the correct initial value
+    });
+  }.observes("i18n.locale"),
+
   actions: {
     switchTool: function(toolKey) {
       this.sendAction('action', toolKey);
