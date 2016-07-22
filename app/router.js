841c7a330e3b54c5539ee2c595b79b914cc0216f
Stefan Kracht <stefankracht@gmail.com>
2016-07-22 13:51:25 +0200

    trigger analytics pageview when path changes

--- app/router.js
+++ app/router.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+   didTransition: function() {
+    this._super(...arguments);
+    return ga('send', 'pageview', {
+      'page': this.get('url'),
+      'title': this.get("currentPath")
+    });
+  }
 });
 
 Router.map(function() {
