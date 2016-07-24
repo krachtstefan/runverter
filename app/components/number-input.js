95d573be01b8c57e247ef6abf210ec3cb441e151
Stefan Kracht <stefankracht@gmail.com>
2016-07-24 16:40:12 +0200

    make number input controlable via up/down arrows

--- app/components/number-input.js
+++ app/components/number-input.js
@@ -3,5 +3,27 @@ export default Ember.Component.extend({
   tagName: "input",
   classNameBindings: ["class"],
   attributeBindings: ['value', 'maxlength', 'type'],
-  type: "tel" // inputs with the type "tel" have the advantage of only allowing numeric values without showing spin buttons as the "number" input does
+  type: "tel", // inputs with the type "tel" have the advantage of only allowing numeric values without showing spin buttons as the "number" input does
+
+  keyDown(event) {
+    switch(event.keyCode) {
+    case 38:
+        this.send('increaseValue');
+        break;
+    case 40:
+        this.send('decreaseValue');
+        break;
+    }
+    return true;
+  },
+
+  actions: {
+    increaseValue() {
+      this.set("value", parseInt(this.get("value"))+1);
+    },
+
+    decreaseValue() {
+      this.set("value", parseInt(this.get("value"))-1);
+    }
+  }
 });
