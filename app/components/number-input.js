d63b4d54633b5fa4ccb96e87fd5ab5642063d2e5
Stefan Kracht <stefankracht@gmail.com>
2016-07-25 11:09:44 +0200

    avoid autocomplete

--- app/components/number-input.js
+++ app/components/number-input.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: "input",
   classNameBindings: ["class"],
-  attributeBindings: ['value', 'maxlength', 'type'],
+  attributeBindings: ['value', 'maxlength', 'type', 'autocomplete'],
   type: "tel", // inputs with the type "tel" have the advantage of only allowing numeric values without showing spin buttons as the "number" input does
+  autocomplete: "off",
 
   input(event) {
     this.set("value", event.target.value);
