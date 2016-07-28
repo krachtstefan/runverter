b2ed99c29d0f7b28892c3c514b3bceee57583963
Stefan Kracht <stefankracht@gmail.com>
2016-07-28 12:15:28 +0200

    handle input event

--- app/components/number-input.js
+++ app/components/number-input.js
@@ -2,12 +2,23 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: "input",
   classNameBindings: ["class"],
-  attributeBindings: ['value', 'maxlength', 'type', 'autocomplete'],
+  attributeBindings: ['value', 'type', 'autocomplete'],
   type: "tel", // inputs with the type "tel" have the advantage of only allowing numeric values without showing spin buttons as the "number" input does
   autocomplete: "off",
 
   input(event) {
-    this.set("value", event.target.value);
+    var newValue = event.target.value;
+    var cursorPosition = event.target.selectionStart; // cursor position starts with 1
+    // if the inputs new value would exceed the allowed maxlength
+    if(newValue.length > parseInt(this.get("maxlength"))){
+      var newValueArr = newValue.split('');
+      var spaceLeft = parseInt(this.get("maxlength"))-cursorPosition; // defines how many positions the curser has left before exceeding the max value
+      var offset = spaceLeft < 0 ? spaceLeft-1 : 0; // use spaceLeft to calculate the offset for the splice function
+      newValueArr.splice(cursorPosition+offset, 1); // extract the part of the old value that, should be overwritten by the new value
+      newValueArr.splice(parseInt(this.get("maxlength"))); // make sure not to exceed the maxlength (fe. if someone pastes multiple characters at once)
+      newValue = newValueArr.join('');
+    }
+    this.set("value", parseInt(newValue));
     return true;
   },
 
