39f083c7b286c09295e1caba254a7ff4aed261ab
Stefan Kracht <stefankracht@gmail.com>
2016-08-05 11:23:28 +0200

    outsource max length in a separate variable

--- app/components/number-input.js
+++ app/components/number-input.js
@@ -8,13 +8,15 @@ export default OneWayTel.extend({
     this._super(...arguments);
     var newValue = event.target.value;
     var cursorPosition = event.target.selectionStart; // cursor position starts with 1
+    var maxLength = parseInt(this.get("maxlength"));
+
     // if the inputs new value would exceed the allowed maxlength
-    if(newValue.length > parseInt(this.get("maxlength"))){
+    if(newValue.length > maxLength){
       var newValueArr = newValue.split('');
-      var spaceLeft = parseInt(this.get("maxlength"))-cursorPosition; // defines how many positions the curser has left before exceeding the max value
+      var spaceLeft = maxLength-cursorPosition; // defines how many positions the curser has left before exceeding the max value
       var offset = spaceLeft < 0 ? spaceLeft-1 : 0; // use spaceLeft to calculate the offset for the splice function
       newValueArr.splice(cursorPosition+offset, 1); // extract the part of the old value that, should be overwritten by the new value
-      newValueArr.splice(parseInt(this.get("maxlength"))); // make sure not to exceed the maxlength (fe. if someone pastes multiple characters at once)
+      newValueArr.splice(maxLength); // make sure not to exceed the maxlength (fe. if someone pastes multiple characters at once)
       newValue = newValueArr.join('');
     }
 
@@ -22,8 +24,8 @@ export default OneWayTel.extend({
     // Example: If the Input contains "112", the max length is 3 and the cursor is at the very end
     // if you type a "2" the new value would still be "112" and ember won't update the value attribute even if there is a binding
     // So the value would still be "112" but the DOM value would be "1122". In this case remove every character that would exceed the max length
-    if(parseInt(this.get("value"))===parseInt(newValue) && this.readDOMAttr('value').length > parseInt(this.get("maxlength"))){
-      this.$().val(this.readDOMAttr('value').slice(0,parseInt(this.get("maxlength"))));
+    if(parseInt(this.get("value"))===parseInt(newValue) && this.readDOMAttr('value').length > maxLength){
+      this.$().val(this.readDOMAttr('value').slice(0,maxLength));
     }
 
     this.set("value", parseInt(newValue));
