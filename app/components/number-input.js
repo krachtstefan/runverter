353cc072439aa4ebe114be8c9975aa3f646313b1
Stefan Kracht <stefankracht@gmail.com>
2016-08-05 11:43:00 +0200

    go back from one way controls to ember textfield

--- app/components/number-input.js
+++ app/components/number-input.js
@@ -1,7 +1,7 @@
-import OneWayTel from './one-way-tel';
 import Ember from 'ember';
-export default OneWayTel.extend({
-  attributeBindings: ['autocomplete'],
+export default Ember.TextField.extend({
+  attributeBindings: ['value:value'], // behaves different than the default "value"
+  type: "tel", // inputs with the type "tel" have the advantage of only allowing numeric values without showing spin buttons as the "number" input does
   autocomplete: "off",
 
   input(event) {
