d6f2f872ccbde7c8199166cb4acd3f92f3714090
Stefan Kracht <stefankracht@gmail.com>
2016-09-21 10:39:21 +0200

    Make lengthMStackDecimalFixed setter deal with 3 digit and negative values

--- app/models/run.js
+++ app/models/run.js
@@ -343,7 +343,15 @@ export default DS.Model.extend({
      * @return {string} new lengthMStackDecimalFixed value
      */
     set: function(propertyName, value) {
-      this.set("lengthMStackDecimal", value);
+      var valueBigNumber = this._ensureBigNumber(value); // don't change value itself, leading zeros may get lost
+
+      // a value like 104 should result in 04 and increment the pre-decimal point position
+      // the same applies for a value like -1, value should be 99 and the pre-decimal point position should be decreased
+      if(valueBigNumber.round().toString().length > this.digits || valueBigNumber.isNegative() === true){
+        this.set("lengthM", this.get("lengthMStackM").plus(valueBigNumber.dividedBy(100)));
+      }else{
+        this.set("lengthMStackDecimal", value);
+      }
       return this.get("lengthMStackDecimalFixedRaw");
     },
   }),
