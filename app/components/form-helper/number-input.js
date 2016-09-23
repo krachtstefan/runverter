3f6a7c3432c105b514f589a22cdd5ee50f3a9bcb
Stefan Kracht <stefankracht@gmail.com>
2016-09-23 15:01:29 +0200

    make variable names more consistent

--- app/components/form-helper/number-input.js
+++ app/components/form-helper/number-input.js
@@ -7,30 +7,30 @@ export default OneWayTel.extend({
 
   input(event) {
     this._super(...arguments);
-    var oldValue = this.readDOMAttr('value');
-    var newValue = oldValue;
+    var currentValue = this.readDOMAttr('value');
+    var nextValue = currentValue;
     var cursorPosition = event.target.selectionStart; // cursor position starts with 1
     var maxLength = parseInt(this.get("maxlength"));
 
     // if the inputs new value would exceed the allowed maxlength
-    if(newValue.length > maxLength){
-      var newValueArr = newValue.split('');
+    if(nextValue.length > maxLength){
+      var nextValueArr = nextValue.split('');
       var spaceLeft = maxLength-cursorPosition; // defines how many positions the curser has left before exceeding the max value
       var offset = spaceLeft < 0 ? spaceLeft-1 : 0; // use spaceLeft to calculate the offset for the splice function
-      newValueArr.splice(cursorPosition+offset, 1); // extract the part of the old value that, should be overwritten by the new value
-      newValueArr.splice(maxLength); // make sure not to exceed the maxlength (fe. if someone pastes multiple characters at once)
-      newValue = newValueArr.join('');
+      nextValueArr.splice(cursorPosition+offset, 1); // extract the part of the old value that, should be overwritten by the new value
+      nextValueArr.splice(maxLength); // make sure not to exceed the maxlength (fe. if someone pastes multiple characters at once)
+      nextValue = nextValueArr.join('');
     }
 
     // Prevent input value from exceeding max length if ember doesn't update the value attribute
     // Example: If the Input contains "112", the max length is 3 and the cursor is at the very end
     // if you type a "2" the new value would still be "112" and ember won't update the value attribute even if there is a binding
     // So the value would still be "112" but the DOM value would be "1122". In this case remove every character that would exceed the max length
-    if(parseInt(this.get("value"))===parseInt(newValue) && oldValue.length > maxLength){
-      this.$().val(oldValue.slice(0,maxLength));
+    if(parseInt(this.get("value"))===parseInt(nextValue) && currentValue.length > maxLength){
+      this.$().val(currentValue.slice(0,maxLength));
     }
 
-    this.set("value", newValue);
+    this.set("value", nextValue);
     return true;
   },
 
