2d570106147d902c56da5209a4079a81895f8686
Stefan Kracht <stefankracht@gmail.com>
2016-09-29 15:40:00 +0200

    add templates to race service

--- app/services/race.js
+++ app/services/race.js
@@ -1,4 +1,35 @@
 import Ember from 'ember';
 export default Ember.Service.extend({
-
+  i18n: Ember.inject.service(),
+  templates : Ember.computed("i18n.locale", function(){
+    return [
+      {
+        'name' : this.get('i18n').t("races.5k"),
+        'lengthM' : new BigNumber(5000)
+      },
+      {
+        'name' : this.get('i18n').t("races.10k"),
+        'lengthM' : new BigNumber(10000)
+      },
+      {
+        'name' : this.get('i18n').t("races.hm"),
+        'lengthM' : new BigNumber(21097.5)
+      },
+      {
+        'name' : this.get('i18n').t("races.m"),
+        'lengthM' : new BigNumber(42195)
+      },
+      {
+        'name' : this.get('i18n').t("races.50k"),
+        'lengthM' : new BigNumber(50000)
+      },
+      {
+        'name' : this.get('i18n').t("races.10k"),
+        'lengthM' : new BigNumber(100000)},
+      {
+        'name' : this.get('i18n').t("races.100m"),
+        'lengthM' : new BigNumber(160934.4)
+      }
+    ];
+  })
 });
