caa088996ff02869dc51475eb64b78f45d2a70bf
Stefan Kracht <stefankracht@gmail.com>
2016-09-30 10:37:21 +0200

    add selectOrDie to race picker

--- app/components/form-helper/race-picker.js
+++ app/components/form-helper/race-picker.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
+import $ from 'jquery';
 export default Ember.Component.extend({
 
+  didInsertElement: function() {
+    this._super(...arguments);
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      $("select."+this.get("id")).selectOrDie({customID: this.get("id") }).ready(function() {
+        $(".menu").selectOrDie("update"); // need to trigger update to select the correct initial value
+      });
+    });
+  }
 });
