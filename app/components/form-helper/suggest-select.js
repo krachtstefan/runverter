cf9de007d562c60296e318fc6eaffc3924f9b4ae
Stefan Kracht <stefankracht@gmail.com>
2016-10-05 14:37:22 +0200

    rename race-picker to suggest-select

--- app/components/form-helper/suggest-select.js
+++ app/components/form-helper/suggest-select.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
 
   i18n: Ember.inject.service(),
 
-  classNames: ["race-picker"],
+  classNames: ["suggest-select"],
 
   didInsertElement: function() {
     this._super(...arguments);
