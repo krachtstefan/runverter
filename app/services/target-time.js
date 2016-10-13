58e02c45ddad6065bf7dd505f58436940f0b03a0
Stefan Kracht <stefankracht@gmail.com>
2016-10-13 11:44:05 +0200

    add some range sample data

--- app/services/target-time.js
+++ app/services/target-time.js
@@ -5,10 +5,14 @@ export default Ember.Service.extend({
     return [
       {
         'name' : "sub 4",
+        'startM' : "9000",
+        'endM' : "11000",
         'timeSec' : new BigNumber(14399)
       },
       {
         'name' : "sub 3",
+        'startM' : "39000",
+        'endM' : "43000",
         'timeSec' : new BigNumber(10799)
       }
     ];
