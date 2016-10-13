d26e1665f3fe3c9b5d6bc4ba84c4c63344f2cdd7
Stefan Kracht <stefankracht@gmail.com>
2016-10-13 15:39:47 +0200

    add marathon target times

--- app/services/target-time.js
+++ app/services/target-time.js
@@ -3,18 +3,49 @@ export default Ember.Service.extend({
   i18n: Ember.inject.service(),
   templates : Ember.computed("i18n.locale", function(){
     return [
+      // marathon
       {
-        'name' : "sub 4",
-        'startM' : "9000",
-        'endM' : "11000",
+        'name' : this.get('i18n').t("targetTimes.sub430"),
+        'startM' : "40000",
+        'endM' : "45000",
+        'timeSec' : new BigNumber(16199)
+      },
+      {
+        'name' : this.get('i18n').t("targetTimes.sub415"),
+        'startM' : "40000",
+        'endM' : "45000",
+        'timeSec' : new BigNumber(15299)
+      },
+      {
+        'name' : this.get('i18n').t("targetTimes.sub4"),
+        'startM' : "40000",
+        'endM' : "45000",
         'timeSec' : new BigNumber(14399)
       },
       {
-        'name' : "sub 3",
-        'startM' : "39000",
-        'endM' : "43000",
+        'name' : this.get('i18n').t("targetTimes.sub345"),
+        'startM' : "40000",
+        'endM' : "45000",
+        'timeSec' : new BigNumber(13499)
+      },
+      {
+        'name' : this.get('i18n').t("targetTimes.sub330"),
+        'startM' : "40000",
+        'endM' : "45000",
+        'timeSec' : new BigNumber(12599)
+      },
+      {
+        'name' : this.get('i18n').t("targetTimes.sub315"),
+        'startM' : "40000",
+        'endM' : "45000",
+        'timeSec' : new BigNumber(11699)
+      },
+      {
+        'name' : this.get('i18n').t("targetTimes.sub3"),
+        'startM' : "40000",
+        'endM' : "45000",
         'timeSec' : new BigNumber(10799)
-      }
+      },
     ];
   })
 });
