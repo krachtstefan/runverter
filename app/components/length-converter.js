a4fbe11dd3633284359f64af5378dce88b1bd3ca
Stefan Kracht <stefankracht@gmail.com>
2016-10-14 14:56:37 +0200

    add tooltip properties to length converter

--- app/components/length-converter.js
+++ app/components/length-converter.js
@@ -1,11 +1,25 @@
 import Ember from 'ember';
 export default Ember.Component.extend({
 
+  i18n: Ember.inject.service(),
+
   racePickerKmVisible : false,
   racePickerMiVisible : false,
   racePickerMVisible : false,
 
   races : Ember.inject.service('race'),
+
+  tooltipLengthKm : Ember.computed("run.lengthKm", "i18n.locale", function(){
+    return this.get("run.lengthKm").round(5).toString().replace(".", this.get('i18n').t("metrics.separator"))+" "+this.get('i18n').t("metrics.distance.km");
+  }),
+
+  tooltipLengthMi : Ember.computed("run.lengthMi", "i18n.locale", function(){
+    return this.get("run.lengthMi").round(5).toString().replace(".", this.get('i18n').t("metrics.separator"))+" "+this.get('i18n').t("metrics.distance.mi");
+  }),
+
+  tooltipLengthM : Ember.computed("run.lengthM", "i18n.locale", function(){
+    return this.get("run.lengthM").round(5).toString().replace(".", this.get('i18n').t("metrics.separator"))+" "+this.get('i18n').t("metrics.distance.m");
+  }),
 
   visible: Ember.computed('selectedMenuItem', function () {
     return this.get("selectedMenuItem.key") === "lc" ? true : false;
