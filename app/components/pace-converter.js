a41c3ba17d9b22d4d13d2d256c7b7ad1ac73c930
Stefan Kracht <stefankracht@gmail.com>
2016-10-14 14:47:57 +0200

    add tooltip properties to pace converter

--- app/components/pace-converter.js
+++ app/components/pace-converter.js
@@ -1,5 +1,24 @@
 import Ember from 'ember';
 export default Ember.Component.extend({
+
+  i18n: Ember.inject.service(),
+
+  tooltipPaceMinPerKm : Ember.computed("run.paceMinPerKm", "i18n.locale", function(){
+    return this.get("run.paceMinPerKm").round(5).toString().replace(".", this.get('i18n').t("metrics.separator"))+" "+this.get('i18n').t("metrics.tempo.minkm");
+  }),
+
+  tooltipPaceMinPerMi : Ember.computed("run.paceMinPerMi", "i18n.locale", function(){
+    return this.get("run.paceMinPerMi").round(5).toString().replace(".", this.get('i18n').t("metrics.separator"))+" "+this.get('i18n').t("metrics.tempo.minmi");
+  }),
+
+  tooltipSpeedKmHr : Ember.computed("run.speedKmHr", "i18n.locale", function(){
+    return this.get("run.speedKmHr").round(5).toString().replace(".", this.get('i18n').t("metrics.separator"))+" "+this.get('i18n').t("metrics.tempo.kmh");
+  }),
+
+  tooltipSpeedMiHr : Ember.computed("run.speedMiHr", "i18n.locale", function(){
+    return this.get("run.speedMiHr").round(5).toString().replace(".", this.get('i18n').t("metrics.separator"))+" "+this.get('i18n').t("metrics.tempo.mih");
+  }),
+
   visible: Ember.computed('selectedMenuItem', function () {
     return this.get("selectedMenuItem.key") === "pc" ? true : false;
   })
