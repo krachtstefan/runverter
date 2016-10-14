fc6209253358e9e3d50767ef350e286096abdb7c
Stefan Kracht <stefankracht@gmail.com>
2016-10-14 09:36:37 +0200

    restructure target time translations

--- app/services/target-time.js
+++ app/services/target-time.js
@@ -5,7 +5,7 @@ export default Ember.Service.extend({
     return [
       // 10k
       {
-        'name' : this.get('i18n').t("targetTimes.sub1"),
+        'name' : this.get('i18n').t("targetTimes.sub1h"),
         'startM' : "9000",
         'endM' : "11000",
         'timeSec' : new BigNumber(3599)
@@ -42,74 +42,74 @@ export default Ember.Service.extend({
       },
       // half marathon
       {
-        'name' : this.get('i18n').t("targetTimes.sub210"),
+        'name' : this.get('i18n').t("targetTimes.sub2h10"),
         'startM' : "20000",
         'endM' : "22000",
         'timeSec' : new BigNumber(7799)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub2"),
+        'name' : this.get('i18n').t("targetTimes.sub2h"),
         'startM' : "20000",
         'endM' : "22000",
         'timeSec' : new BigNumber(7199)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub150"),
+        'name' : this.get('i18n').t("targetTimes.sub1h50"),
         'startM' : "20000",
         'endM' : "22000",
         'timeSec' : new BigNumber(6599)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub140"),
+        'name' : this.get('i18n').t("targetTimes.sub1h40"),
         'startM' : "20000",
         'endM' : "22000",
         'timeSec' : new BigNumber(5999)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub130"),
+        'name' : this.get('i18n').t("targetTimes.sub1h30"),
         'startM' : "20000",
         'endM' : "22000",
         'timeSec' : new BigNumber(5399)
       },
       // marathon (and 50k)
       {
-        'name' : this.get('i18n').t("targetTimes.sub430"),
+        'name' : this.get('i18n').t("targetTimes.sub4h30"),
         'startM' : "40000",
         'endM' : "51000",
         'timeSec' : new BigNumber(16199)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub415"),
+        'name' : this.get('i18n').t("targetTimes.sub4h15"),
         'startM' : "40000",
         'endM' : "51000",
         'timeSec' : new BigNumber(15299)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub4"),
+        'name' : this.get('i18n').t("targetTimes.sub4h"),
         'startM' : "40000",
         'endM' : "51000",
         'timeSec' : new BigNumber(14399)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub345"),
+        'name' : this.get('i18n').t("targetTimes.sub3h45"),
         'startM' : "40000",
         'endM' : "51000",
         'timeSec' : new BigNumber(13499)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub330"),
+        'name' : this.get('i18n').t("targetTimes.sub3h30"),
         'startM' : "40000",
         'endM' : "51000",
         'timeSec' : new BigNumber(12599)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub315"),
+        'name' : this.get('i18n').t("targetTimes.sub3h15"),
         'startM' : "40000",
         'endM' : "45000",
         'timeSec' : new BigNumber(11699)
       },
       {
-        'name' : this.get('i18n').t("targetTimes.sub3"),
+        'name' : this.get('i18n').t("targetTimes.sub3h"),
         'startM' : "40000",
         'endM' : "45000",
         'timeSec' : new BigNumber(10799)
