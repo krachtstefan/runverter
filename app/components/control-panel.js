cd6d1a1d1fe4fa369599554b99ca80821898648f
Stefan Kracht <stefankracht@gmail.com>
2016-10-24 15:01:40 +0200

    add imprintVisible to control panel component

--- app/components/control-panel.js
+++ app/components/control-panel.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
   classNames: ["control-panel"],
   classNameBindings: ['visible:open'],
   visible : false,
+  imprintVisible : false,
 
   actions: {
     toggleControlPanel: function() {
