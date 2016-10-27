74ddd381ba1677678fc8d426abacb878b0e8cb5a
Stefan Kracht <stefankracht@gmail.com>
2016-10-27 09:16:04 +0200

    use ' instead of "

--- app/locales/de/translations.js
+++ app/locales/de/translations.js
@@ -1,120 +1,120 @@
 export default {
-  "controlPanel" : {
-    "switchLanguage" : "Switch to english",
-    "enableExpertMode" : "Expertenmodus aktivieren",
-    "disableExpertMode" : "Expertenmodus deaktivieren",
-    "share" : "Erzähle es deiner Laufcrew",
-    "learnMore" : "Mehr über Runverter erfahren",
-    "feedback" : "Twitter mir dein Feedback"
+  'controlPanel' : {
+    'switchLanguage' : 'Switch to english',
+    'enableExpertMode' : 'Expertenmodus aktivieren',
+    'disableExpertMode' : 'Expertenmodus deaktivieren',
+    'share' : 'Erzähle es deiner Laufcrew',
+    'learnMore' : 'Mehr über Runverter erfahren',
+    'feedback' : 'Twitter mir dein Feedback'
   },
-  "tools" : {
-    "pca" : {
-      "name" : "Tempo ermittlen",
-      "label" : "Hi! Hier kannst du dein optimales",
-      "description" : "Finde heraus wie schnell du sein musst, um eine Strecke in gewünschter Zeit zu schaffen.",
-      "distanceLabel" : "Für eine Strecke von",
-      "timeLabel" : "in",
-      "paceLabel" : "benötigst du eine Geschwindigkeit von"
+  'tools' : {
+    'pca' : {
+      'name' : 'Tempo ermittlen',
+      'label' : 'Hi! Hier kannst du dein optimales',
+      'description' : 'Finde heraus wie schnell du sein musst, um eine Strecke in gewünschter Zeit zu schaffen.',
+      'distanceLabel' : 'Für eine Strecke von',
+      'timeLabel' : 'in',
+      'paceLabel' : 'benötigst du eine Geschwindigkeit von'
     },
-    "pc" : {
-      "name" : "Tempo umrechnen",
-      "label" : "Ich helfe dir beim",
-      "description" : "Es gibt viele Arten deine Geschwindigkeit anzugeben. Hier hast du alle im Blick.",
-      "minkmLabel" : "Eine Pace von",
-      "minmiLabel" : "oder",
-      "kmhLabel" : "entspricht einer Geschwindigkeit von",
-      "mihLabel" : "bzw."
+    'pc' : {
+      'name' : 'Tempo umrechnen',
+      'label' : 'Ich helfe dir beim',
+      'description' : 'Es gibt viele Arten deine Geschwindigkeit anzugeben. Hier hast du alle im Blick.',
+      'minkmLabel' : 'Eine Pace von',
+      'minmiLabel' : 'oder',
+      'kmhLabel' : 'entspricht einer Geschwindigkeit von',
+      'mihLabel' : 'bzw.'
     },
-    "lc" : {
-      "name" : "Länge umrechnen",
-      "label" : "Du kannst hier die",
-      "description" : "Hier findest du die wichtigsten Längenangaben. Und ja, die Länge ist entscheidend.",
-      "kmLabel" : "Eine Strecke von",
-      "miLabel" : "entspricht",
-      "mLabel" : "bzw."
+    'lc' : {
+      'name' : 'Länge umrechnen',
+      'label' : 'Du kannst hier die',
+      'description' : 'Hier findest du die wichtigsten Längenangaben. Und ja, die Länge ist entscheidend.',
+      'kmLabel' : 'Eine Strecke von',
+      'miLabel' : 'entspricht',
+      'mLabel' : 'bzw.'
     },
-    "rp" : {
-      "name" : "Wettkampfprognose",
-      "label" : "Dieser Platz ist reserviert für die",
-      "description" : "Noch nicht ganz fertig..."
+    'rp' : {
+      'name' : 'Wettkampfprognose',
+      'label' : 'Dieser Platz ist reserviert für die',
+      'description' : 'Noch nicht ganz fertig...'
     },
-    "sc" : {
-      "name" : "Zwischenzeit-Rechner",
-      "label" : "Das ist nicht der",
-      "description" : "Aber hier wird es ihn bald geben. Versprochen."
+    'sc' : {
+      'name' : 'Zwischenzeit-Rechner',
+      'label' : 'Das ist nicht der',
+      'description' : 'Aber hier wird es ihn bald geben. Versprochen.'
     }
   },
-  "metrics" : {
-    "separator" : ",",
-    "distance" : {
-      "separator" : ",",
-      "km" : "km",
-      "mi" : "mi",
-      "m" : "m"
+  'metrics' : {
+    'separator' : ',',
+    'distance' : {
+      'separator' : ',',
+      'km' : 'km',
+      'mi' : 'mi',
+      'm' : 'm'
     },
-    "tempo" : {
-      "separator" : ":",
-      "minkm" : "min/km",
-      "minmi" : "min/mi",
-      "kmh" : "km/h",
-      "mih" : "mi/h",
+    'tempo' : {
+      'separator' : ':',
+      'minkm' : 'min/km',
+      'minmi' : 'min/mi',
+      'kmh' : 'km/h',
+      'mih' : 'mi/h',
     },
-    "time" : {
-      "separator" : ":",
-      "hr" : "Std",
-      "min" : "Min",
-      "sec" : "Sek",
+    'time' : {
+      'separator' : ':',
+      'hr' : 'Std',
+      'min' : 'Min',
+      'sec' : 'Sek',
     }
   },
-  "races" : {
-    "label" : "Wähle ein Wettkampf:",
-    "10k" : "10k",
-    "hm" : "Halbmarathon",
-    "m" : "Marathon",
-    "50k" : "50k",
-    "50m" : "50 Meilen",
-    "100k" : "100k",
-    "100m" : "100 Meilen",
+  'races' : {
+    'label' : 'Wähle ein Wettkampf:',
+    '10k' : '10k',
+    'hm' : 'Halbmarathon',
+    'm' : 'Marathon',
+    '50k' : '50k',
+    '50m' : '50 Meilen',
+    '100k' : '100k',
+    '100m' : '100 Meilen',
   },
-  "targetTimes" : {
-    "label" : "Wähle eine Wunschzeit:",
-    "sub23h" : "unter 23:00",
-    "sub22h" : "unter 22:00",
-    "sub21h" : "unter 21:00",
-    "sub20h" : "unter 20:00",
-    "sub19h" : "unter 19:00",
-    "sub18h" : "unter 18:00",
-    "sub15h" : "unter 15:00",
-    "sub14h" : "unter 14:00",
-    "sub13h" : "unter 13:00",
-    "sub12h" : "unter 12:00",
-    "sub11h" : "unter 11:00",
-    "sub10h" : "unter 10:00",
-    "sub9h" : "unter 9:00",
-    "sub8h" : "unter 8:00",
-    "sub7h" : "unter 7:00",
-    "sub6h" : "unter 6:00",
-    "sub5h" : "unter 5:00",
-    "sub4h30" : "unter 4:30",
-    "sub4h15" : "unter 4:15",
-    "sub4h" : "unter 4:00",
-    "sub3h45" : "unter 3:45",
-    "sub3h30" : "unter 3:30",
-    "sub3h15" : "unter 3:15",
-    "sub3h" : "unter 3:00",
-    "sub2h10" : "unter 2:10",
-    "sub2h" : "unter 2:00",
-    "sub1h50" : "unter 1:50",
-    "sub1h40" : "unter 1:40",
-    "sub1h30" : "unter 1:30",
-    "sub1h" : "unter 1:00",
-    "sub55" : "unter 0:55",
-    "sub50" : "unter 0:50",
-    "sub45" : "unter 0:45",
-    "sub40" : "unter 0:40",
-    "sub35" : "unter 0:35",
+  'targetTimes' : {
+    'label' : 'Wähle eine Wunschzeit:',
+    'sub23h' : 'unter 23:00',
+    'sub22h' : 'unter 22:00',
+    'sub21h' : 'unter 21:00',
+    'sub20h' : 'unter 20:00',
+    'sub19h' : 'unter 19:00',
+    'sub18h' : 'unter 18:00',
+    'sub15h' : 'unter 15:00',
+    'sub14h' : 'unter 14:00',
+    'sub13h' : 'unter 13:00',
+    'sub12h' : 'unter 12:00',
+    'sub11h' : 'unter 11:00',
+    'sub10h' : 'unter 10:00',
+    'sub9h' : 'unter 9:00',
+    'sub8h' : 'unter 8:00',
+    'sub7h' : 'unter 7:00',
+    'sub6h' : 'unter 6:00',
+    'sub5h' : 'unter 5:00',
+    'sub4h30' : 'unter 4:30',
+    'sub4h15' : 'unter 4:15',
+    'sub4h' : 'unter 4:00',
+    'sub3h45' : 'unter 3:45',
+    'sub3h30' : 'unter 3:30',
+    'sub3h15' : 'unter 3:15',
+    'sub3h' : 'unter 3:00',
+    'sub2h10' : 'unter 2:10',
+    'sub2h' : 'unter 2:00',
+    'sub1h50' : 'unter 1:50',
+    'sub1h40' : 'unter 1:40',
+    'sub1h30' : 'unter 1:30',
+    'sub1h' : 'unter 1:00',
+    'sub55' : 'unter 0:55',
+    'sub50' : 'unter 0:50',
+    'sub45' : 'unter 0:45',
+    'sub40' : 'unter 0:40',
+    'sub35' : 'unter 0:35',
   },
-  "imprint" : {
-    "title" : "Impressum"
+  'imprint' : {
+    'title' : 'Impressum'
   }
 };
--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -1,120 +1,120 @@
 export default {
-  "controlPanel" : {
-    "switchLanguage" : "Deutsche Version",
-    "enableExpertMode" : "Enable expert mode",
-    "disableExpertMode" : "Disable expert mode",
-    "share" : "Share with your running crew",
-    "learnMore" : "Learn more about runverter",
-    "feedback" : "Tweet me your feedback"
+  'controlPanel' : {
+    'switchLanguage' : 'Deutsche Version',
+    'enableExpertMode' : 'Enable expert mode',
+    'disableExpertMode' : 'Disable expert mode',
+    'share' : 'Share with your running crew',
+    'learnMore' : 'Learn more about runverter',
+    'feedback' : 'Tweet me your feedback'
   },
-  "tools" : {
-    "pca" : {
-      "name" : "Pace Calculator",
-      "label" : "Hi! I'm the",
-      "description" : "See which pace you need, to finish a run in your desired time.",
-      "distanceLabel" : "Running a distance of",
-      "timeLabel" : "in",
-      "paceLabel" : "requires a pace of"
+  'tools' : {
+    'pca' : {
+      'name' : 'Pace Calculator',
+      'label' : 'Hi! I\'m the',
+      'description' : 'See which pace you need, to finish a run in your desired time.',
+      'distanceLabel' : 'Running a distance of',
+      'timeLabel' : 'in',
+      'paceLabel' : 'requires a pace of'
     },
-    "pc" : {
-      "name" : "Pace Converter",
-      "label" : "You're using the",
-      "description" : "There a lots of ways to define a tempo. Here you have all in one place.",
-      "minkmLabel" : "A pace of",
-      "minmiLabel" : "or",
-      "kmhLabel" : "equals a speed of",
-      "mihLabel" : "or"
+    'pc' : {
+      'name' : 'Pace Converter',
+      'label' : 'You\'re using the',
+      'description' : 'There a lots of ways to define a tempo. Here you have all in one place.',
+      'minkmLabel' : 'A pace of',
+      'minmiLabel' : 'or',
+      'kmhLabel' : 'equals a speed of',
+      'mihLabel' : 'or'
     },
-    "lc" : {
-      "name" : "Lenght Converter",
-      "label" : "This is the",
-      "description" : "Here you can see the relation of all common length units. And yes, length matters.",
-      "kmLabel" : "A distance of",
-      "miLabel" : "equals a distance of",
-      "mLabel" : "or"
+    'lc' : {
+      'name' : 'Lenght Converter',
+      'label' : 'This is the',
+      'description' : 'Here you can see the relation of all common length units. And yes, length matters.',
+      'kmLabel' : 'A distance of',
+      'miLabel' : 'equals a distance of',
+      'mLabel' : 'or'
     },
-    "rp" : {
-      "name" : "Race Predictor",
-      "label" : "This space is reserved for the",
-      "description" : "Not quite ready yet..."
+    'rp' : {
+      'name' : 'Race Predictor',
+      'label' : 'This space is reserved for the',
+      'description' : 'Not quite ready yet...'
     },
-    "sc" : {
-      "name" : "Split Time Calculator",
-      "label" : "Unfortunately this is not the",
-      "description" : "This feature will come pretty soon. I promise."
+    'sc' : {
+      'name' : 'Split Time Calculator',
+      'label' : 'Unfortunately this is not the',
+      'description' : 'This feature will come pretty soon. I promise.'
     }
   },
-  "metrics" : {
-    "separator" : ".",
-    "distance" : {
-      "separator" : ".",
-      "km" : "km",
-      "mi" : "mi",
-      "m" : "m"
+  'metrics' : {
+    'separator' : '.',
+    'distance' : {
+      'separator' : '.',
+      'km' : 'km',
+      'mi' : 'mi',
+      'm' : 'm'
     },
-    "tempo" : {
-      "separator" : ":",
-      "minkm" : "min/km",
-      "minmi" : "min/mi",
-      "kmh" : "km/h",
-      "mih" : "mi/h",
+    'tempo' : {
+      'separator' : ':',
+      'minkm' : 'min/km',
+      'minmi' : 'min/mi',
+      'kmh' : 'km/h',
+      'mih' : 'mi/h',
     },
-    "time" : {
-      "separator" : ":",
-      "hr" : "h",
-      "min" : "min",
-      "sec" : "sec",
+    'time' : {
+      'separator' : ':',
+      'hr' : 'h',
+      'min' : 'min',
+      'sec' : 'sec',
     }
   },
-  "races" : {
-    "label" : "Select a race:",
-    "10k" : "10k",
-    "hm" : "Half Marathon",
-    "m" : "Marathon",
-    "50k" : "50k",
-    "50m" : "50 Miles",
-    "100k" : "100k",
-    "100m" : "100 Miles",
+  'races' : {
+    'label' : 'Select a race:',
+    '10k' : '10k',
+    'hm' : 'Half Marathon',
+    'm' : 'Marathon',
+    '50k' : '50k',
+    '50m' : '50 Miles',
+    '100k' : '100k',
+    '100m' : '100 Miles',
   },
-  "targetTimes" : {
-    "label" : "Select a time goal:",
-    "sub23h" : "sub 23:00",
-    "sub22h" : "sub 22:00",
-    "sub21h" : "sub 21:00",
-    "sub20h" : "sub 20:00",
-    "sub19h" : "sub 19:00",
-    "sub18h" : "sub 18:00",
-    "sub15h" : "sub 15:00",
-    "sub14h" : "sub 14:00",
-    "sub13h" : "sub 13:00",
-    "sub12h" : "sub 12:00",
-    "sub11h" : "sub 11:00",
-    "sub10h" : "sub 10:00",
-    "sub9h" : "sub 9:00",
-    "sub8h" : "sub 8:00",
-    "sub7h" : "sub 7:00",
-    "sub6h" : "sub 6:00",
-    "sub5h" : "sub 5:00",
-    "sub4h30" : "sub 4:30",
-    "sub4h15" : "sub 4:15",
-    "sub4h" : "sub 4:00",
-    "sub3h45" : "sub 3:45",
-    "sub3h30" : "sub 3:30",
-    "sub3h15" : "sub 3:15",
-    "sub3h" : "sub 3:00",
-    "sub2h10" : "sub 2:10",
-    "sub2h" : "sub 2:00",
-    "sub1h50" : "sub 1:50",
-    "sub1h40" : "sub 1:40",
-    "sub1h30" : "sub 1:30",
-    "sub1h" : "sub 1:00",
-    "sub55" : "sub 0:55",
-    "sub50" : "sub 0:50",
-    "sub45" : "sub 0:45",
-    "sub40" : "sub 0:40",
-    "sub35" : "sub 0:35",
+  'targetTimes' : {
+    'label' : 'Select a time goal:',
+    'sub23h' : 'sub 23:00',
+    'sub22h' : 'sub 22:00',
+    'sub21h' : 'sub 21:00',
+    'sub20h' : 'sub 20:00',
+    'sub19h' : 'sub 19:00',
+    'sub18h' : 'sub 18:00',
+    'sub15h' : 'sub 15:00',
+    'sub14h' : 'sub 14:00',
+    'sub13h' : 'sub 13:00',
+    'sub12h' : 'sub 12:00',
+    'sub11h' : 'sub 11:00',
+    'sub10h' : 'sub 10:00',
+    'sub9h' : 'sub 9:00',
+    'sub8h' : 'sub 8:00',
+    'sub7h' : 'sub 7:00',
+    'sub6h' : 'sub 6:00',
+    'sub5h' : 'sub 5:00',
+    'sub4h30' : 'sub 4:30',
+    'sub4h15' : 'sub 4:15',
+    'sub4h' : 'sub 4:00',
+    'sub3h45' : 'sub 3:45',
+    'sub3h30' : 'sub 3:30',
+    'sub3h15' : 'sub 3:15',
+    'sub3h' : 'sub 3:00',
+    'sub2h10' : 'sub 2:10',
+    'sub2h' : 'sub 2:00',
+    'sub1h50' : 'sub 1:50',
+    'sub1h40' : 'sub 1:40',
+    'sub1h30' : 'sub 1:30',
+    'sub1h' : 'sub 1:00',
+    'sub55' : 'sub 0:55',
+    'sub50' : 'sub 0:50',
+    'sub45' : 'sub 0:45',
+    'sub40' : 'sub 0:40',
+    'sub35' : 'sub 0:35',
   },
-  "imprint" : {
-    "title" : "Impressum"
+  'imprint' : {
+    'title' : 'Impressum'
   }
 };
