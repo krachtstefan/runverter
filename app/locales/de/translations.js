cee3351a6a5de9aeebf97159018ee985f9d9439a
Stefan Kracht <stefankracht@gmail.com>
2016-11-14 11:55:17 +0100

    add DescriptionTag to translations

--- app/locales/de/translations.js
+++ app/locales/de/translations.js
@@ -118,6 +118,7 @@ export default {
   'imprint' : {
     'about' : 'Über Runverter',
     'aboutText' : 'Runverter.io ist ein Projekt von <a href="http://stefankracht.de/" target="_blank">Stefan Kracht</a>. Mehr Infos gibt es in einem <a href="http://stefankracht.de/news/runverter" target="_blank">ausführlichen Blog-Post</a>.',
+    'DescriptionTag' : 'Runverter.io ist das schweizer Taschenmesser für Läufer, erstellt von Stefan Kracht',
     'thanks' : 'Dankeschön ♥',
     'thanksText' : 'Diese App läuft mit <a href="http://emberjs.com/" target="_blank">Ember.js</a>. Die meisten Icons stammen von den <a href="https://thenounproject.com/" target="_blank">The Noun Project</a> Usern <a href="https://thenounproject.com/term/speech-bubble/358344/" target="_blank">lipi</a>, <a href="https://thenounproject.com/term/happy/350362/" target="_blank">Bernadette Little</a>, <a href="https://thenounproject.com/term/glasses/31753/" target="_blank">Simon Child</a>, <a href="https://thenounproject.com/term/share/375368/" target="_blank">Aysgl Avcu</a> and <a href="https://thenounproject.com/term/information/222828/" target="_blank">Douglas Santos</a>.',
     'responsible' :'Verantwortlich für den Inhalt',
