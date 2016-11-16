e6526ca22da271a5ff1dcf49dd04cc261039bae5
Stefan Kracht <stefankracht@gmail.com>
2016-11-16 12:09:47 +0100

    Revert "ember init"
    This reverts commit 920dd97301307d4d075c31aacadf77de33610d8f.

--- app/router.js
+++ app/router.js
@@ -2,10 +2,20 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+   didTransition: function() {
+    this._super(...arguments);
+    return ga('send', 'pageview', {
+      'page': this.get('url'),
+      'title': this.get("currentPath")
+    });
+  }
 });
 
 Router.map(function() {
+  this.resource('tools', { path: '/' }, function(){
+    this.route('locale', { path: '/:locale' });
+  });
 });
 
 export default Router;
