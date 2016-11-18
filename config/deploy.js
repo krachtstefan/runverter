77678724275d9f0ffe6610dd99b4822194e2d2e3
Stefan Kracht <stefankracht@gmail.com>
2016-11-18 14:28:22 +0100

    more compact deploy config

--- config/deploy.js
+++ config/deploy.js
@@ -1,10 +1,11 @@
 module.exports = function(environment){
-  var ENV = {};
-  ENV['s3'] = {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    bucket: 'runverter',
-    region: 'eu-west-1'
-  }
+  var ENV = {
+    's3' : {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      bucket: 'runverter',
+      region: 'eu-west-1'
+    }
+  };
   return ENV;
 };
