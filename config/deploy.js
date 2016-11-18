e45cc1ce8d5e5be1680c46890c15223f4f6ec425
Stefan Kracht <stefankracht@gmail.com>
2016-11-18 14:37:20 +0100

    add redis url

--- config/deploy.js
+++ config/deploy.js
@@ -1,5 +1,8 @@
 module.exports = function(environment){
   var ENV = {
+    'redis' : {
+        url: 'redis://runverter.staging:6379',
+    },
     's3' : {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
