c890b628b087883c7a262c901e29393d33a46c91
Stefan Kracht <stefankracht@gmail.com>
2016-11-18 15:41:40 +0100

    update deploy.js

--- config/deploy.js
+++ config/deploy.js
@@ -1,21 +1,33 @@
-module.exports = function(environment){
+var VALID_DEPLOY_TARGETS = [
+  'production',
+  'staging',
+];
 
-  if(environment === 'production') {
+module.exports = function(deployTarget) {
+
+  if(deployTarget === 'production') {
     var redisUrl = 'redis://runverter.production:6380'
-  }else if(environment === 'staging') {
+  }else if(deployTarget === 'staging') {
     var redisUrl = 'redis://runverter.staging:6379'
   }
 
   var ENV = {
-    'redis' : {
-        url: redisUrl,
+    build: {},
+    redis : {
+      allowOverwrite: true,
+      keyPrefix: 'runverter:index',
+      url: redisUrl,
     },
-    's3' : {
+    s3 : {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       bucket: 'runverter',
       region: 'eu-west-1'
     }
   };
+
+  if (VALID_DEPLOY_TARGETS.indexOf(deployTarget) === -1) {
+    throw new Error('Invalid deployTarget ' + deployTarget);
+  }
   return ENV;
 };
