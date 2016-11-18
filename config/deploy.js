43d0612d2b6390bc4c3df2703efc38f2ed629d82
Stefan Kracht <stefankracht@gmail.com>
2016-11-18 14:16:06 +0100

    update deploy.js to newest version

--- config/deploy.js
+++ config/deploy.js
@@ -1,34 +1,10 @@
-module.exports = {
-  staging: {
-    buildEnv: 'staging',
-    store: {
-      type: 'redis',
-      host: 'runverter.staging',
-      port: 6379
-    },
-    assets: {
-      type: 's3',
-      exclude: ['.DS_Store', '*-test.js'],
-      accessKeyId: 'AKIAJQH4RE42AHIISVXQ',
-      secretAccessKey: process.env['AWS_ACCESS_KEY'],
-      bucket: 'runverter',
-      region: "eu-west-1"
-    }
-  },
-  production: {
-    buildEnv: 'production',
-    store: {
-      type: 'redis',
-      host: 'localhost',
-      port: 6380
-    },
-    assets: {
-      type: 's3',
-      exclude: ['.DS_Store', '*-test.js'],
-      accessKeyId: 'AKIAJQH4RE42AHIISVXQ',
-      secretAccessKey: process.env['AWS_ACCESS_KEY'],
-      bucket: 'runverter',
-      region: "eu-west-1"
-    }
+module.exports = function(environment){
+  var ENV = {};
+  ENV['s3'] = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    bucket: 'runverter',
+    region: 'eu-west-1'
   }
+  return ENV;
 };
