ce708ce060146f3fafcd2ab4258121b7669eb771
Stefan Kracht <stefankracht@gmail.com>
2016-12-22 14:14:59 +0100

    add test to ensure prediction to be a valid data model

--- tests/unit/models/prediction-test.js
+++ tests/unit/models/prediction-test.js
@@ -3,3 +3,9 @@ import Ember from 'ember';
 import { test, moduleForModel } from 'ember-qunit';
 
 moduleForModel('run', 'Prediction Model');
+
+test('prediction is a valid ember data Model', function(assert) {
+  var prediction = this.subject();
+  assert.ok(prediction);
+  assert.ok(prediction instanceof DS.Model);
+});
