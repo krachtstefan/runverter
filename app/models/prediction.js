1c68408c0547a17964a8387439a308576f33129d
Stefan Kracht <stefankracht@gmail.com>
2016-12-22 16:35:04 +0100

    add achievedRun and predictedRun to prediction model

--- app/models/prediction.js
+++ app/models/prediction.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 BigNumber.config({DECIMAL_PLACES: 25});
 export default DS.Model.extend({
 
+  /**
+   * achievedRun represents the achieved run, needed to calcuate the predicted run
+   */
+  achievedRun: DS.belongsTo('run'),
+
+  /**
+   * predictedRun represents the predicted run, calculated from achieved run
+   */
+  predictedRun: DS.belongsTo('run')
 
 });
