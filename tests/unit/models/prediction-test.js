0fa1e15312e6da463859cdd1f1ccf5a1f14318f0
Stefan Kracht <stefankracht@gmail.com>
2016-12-28 09:35:18 +0100

    test achievedRun and predictedRun behaviour

--- tests/unit/models/prediction-test.js
+++ tests/unit/models/prediction-test.js
@@ -21,6 +21,19 @@ test('achievedRun is a relation to run model', function(assert) {
   assert.equal(relationship.type, 'run');
 });
 
+test('achievedRun behaves like run model', function(assert) {
+  var prediction = this.subject(), self = this;
+  Ember.run(function(){
+    prediction.set('achievedRun',
+      self.store().createRecord('run',{
+        timeSec : new BigNumber(3600*4),
+        lengthM : new BigNumber(42195)
+      })
+    );
+  });
+  assert.strictEqual(prediction.get("achievedRun.timeHrRaw").toString(), "4");
+});
+
 // predictedRun
 test('predictedRun is a relation to run model', function(assert) {
   const prediction = this.store().modelFor('prediction');
@@ -28,4 +41,17 @@ test('predictedRun is a relation to run model', function(assert) {
   assert.equal(relationship.key, 'predictedRun');
   assert.equal(relationship.kind, 'belongsTo');
   assert.equal(relationship.type, 'run');
+});
+
+test('predictedRun behaves like run model', function(assert) {
+  var prediction = this.subject(), self = this;
+  Ember.run(function(){
+    prediction.set('predictedRun',
+      self.store().createRecord('run',{
+        timeSec : new BigNumber(3600*4),
+        lengthM : new BigNumber(42195)
+      })
+    );
+  });
+  assert.strictEqual(prediction.get("predictedRun.timeHrRaw").toString(), "4");
 });
