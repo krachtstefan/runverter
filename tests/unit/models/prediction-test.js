cc430fd8ae23e106f55763e4ab4773f901133fbc
Stefan Kracht <stefankracht@gmail.com>
2017-01-03 17:15:00 +0100

    add test for peterRiegelMethod and peterRiegelMethodVanilla

--- tests/unit/models/prediction-test.js
+++ tests/unit/models/prediction-test.js
@@ -94,3 +94,16 @@ test('predictedRun changes when achievedRun time does', function(assert) {
 test('predictedRun changes when achievedRun length does', function(assert) {
   assert.strictEqual(true, true);
 });
+
+// peterRiegelMethod
+test('test peterRiegelMethod', function(assert) {
+  var prediction = this.subject();
+  //104.2465760841121 5
+  //104.2465760841121 390955
+  assert.strictEqual(prediction.peterRiegelMethod(10, 20, 50).toString(), "104.24657608411214"); // 1:44:15
+});
+
+test('test peterRiegelMethodVanilla', function(assert) {
+  var prediction = this.subject();
+  assert.strictEqual(prediction.peterRiegelMethodVanilla(10, 20, 50).toString(), "104.24657608411214"); // 1:44:15
+});
