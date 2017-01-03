4795baab1f7c67639f9f3e1e41c4cfa1d5096d3a
Stefan Kracht <stefankracht@gmail.com>
2017-01-03 14:47:01 +0100

    copy _ensureBigNumber method from run model

--- app/models/prediction.js
+++ app/models/prediction.js
@@ -29,4 +29,18 @@ export default DS.Model.extend({
     this.get("predictedRun").set("timeSec", this.get("achievedRun.timeSec"));
   }),
 
+  /**
+   * will convert the input to BigNumber if necessary. If input is BigNumber already
+   * it will be left unchanged. This method is handy for setter methods of this class.
+   * Setter may be called from user input (string) or other methods of this class which
+   * already provide Bignumber. In the second case, it is important to keep the BigNumber
+   * type to prevent precision loss
+   *
+   * @param  {BigNumber|string|number} input  any number like input
+   * @return {BigNumber} output instance of BigNumber
+   */
+  _ensureBigNumber : function(input){
+    return (input instanceof BigNumber) ? input : new BigNumber(+input || 0);
+  }
+
 });
