b7242811d4cb8f7880321b7cf07d0dc5375d8cfb
Stefan Kracht <stefankracht@gmail.com>
2017-01-05 14:07:24 +0100

    define visible property

--- app/components/race-predictor.js
+++ app/components/race-predictor.js
@@ -1,4 +1,9 @@
+import Ember from 'ember';
 export default Ember.Component.extend({
 
   i18n: Ember.inject.service(),
+
+  visible: Ember.computed('selectedMenuItem', function () {
+    return this.get("selectedMenuItem.key") === "rp" ? true : false;
+  }),
 });
