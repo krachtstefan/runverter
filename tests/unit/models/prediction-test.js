e17be049e39476dddaf5a573a34b648e9b9210a8
Stefan Kracht <stefankracht@gmail.com>
2017-01-06 08:57:19 +0100

    fix typo

--- tests/unit/models/prediction-test.js
+++ tests/unit/models/prediction-test.js
@@ -174,16 +174,16 @@ test('peterRiegelMethod works (with integers)', function(assert) {
   assert.strictEqual(prediction.peterRiegelMethod(10, 20, 50).toString(), "104.2465760841121391"); // 104.2465760841121390955
 });
 
-test('peterRiegelMethod works (with String)', function(assert) {
+test('peterRiegelMethod works (with Strings)', function(assert) {
   var prediction = this.subject();
   // 1 mi in 0:08:15, 5 mi? = 45:26
   assert.strictEqual(prediction.peterRiegelMethod("1.609344", "8.04672", "8.25" ).toString(), "45.432031118731957482"); // 45.43203111873195748203
 });
 
-test('peterRiegelMethod works (with BigNumber)', function(assert) {
+test('peterRiegelMethod works (with BigNumbers)', function(assert) {
   var prediction = this.subject();
   // Marathon in 3:26:44, Half Marathon? = 1:39:10 (runcalc.net says 1:39:09 but it's not rounded properly)
-  assert.strictEqual(prediction.peterRiegelMethod(new BigNumber(42.195), new BigNumber(21.0975), new BigNumber(44).div(60).plus(206)).toString(), "99.155934467588156066"); // 99.15593446758816246001 (does not match precisely but good enoguh)
+  assert.strictEqual(prediction.peterRiegelMethod(new BigNumber(42.195), new BigNumber(21.0975), new BigNumber(44).div(60).plus(206)).toString(), "99.155934467588156066"); // 99.15593446758816246001 (does not match precisely but good enough)
 });
 
 test('peterRiegelMethod will predict the same time if input and output length are equal ', function(assert) {
