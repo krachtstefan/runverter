f631196c5a9c907cb2f75c9c5bb4bbbd24d14884
Stefan Kracht <stefankracht@gmail.com>
2017-01-11 08:27:18 +0100

    add properties to handle race and timepicker visibility

--- app/components/race-predictor.js
+++ app/components/race-predictor.js
@@ -2,10 +2,35 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   i18n: Ember.inject.service(),
+  achievedRacePickerVisible: false,
+  achievedTimePickerVisible: false,
+  predictedRacePickerVisible: false,
+  predictedTimePickerVisible: false,
+
+  isTouchDevice : Ember.computed(function(){
+    return 'ontouchstart' in document.documentElement;
+  }),
 
   visible: Ember.computed('selectedMenuItem', function () {
     return this.get("selectedMenuItem.key") === "rp" ? true : false;
   }),
+
+  achievedRacePickerVisibleClass: Ember.computed('achievedRacePickerVisible', function () {
+    return this.get("achievedRacePickerVisible") === true || this.get("isTouchDevice") === true ? "suggestSelectVisible" : "suggestSelectInvisible";
+  }),
+
+  achievedTimePickerVisibleClass: Ember.computed('achievedTimePickerVisible', function () {
+    return this.get("achievedTimePickerVisible") === true || this.get("isTouchDevice") === true ? "suggestSelectVisible" : "suggestSelectInvisible";
+  }),
+
+  predictedRacePickerVisibleClass: Ember.computed('predictedRacePickerVisible', function () {
+    return this.get("predictedRacePickerVisible") === true || this.get("isTouchDevice") === true ? "suggestSelectVisible" : "suggestSelectInvisible";
+  }),
+
+  predictedTimePickerVisibleClass: Ember.computed('predictedTimePickerVisible', function () {
+    return this.get("predictedTimePickerVisible") === true || this.get("isTouchDevice") === true ? "suggestSelectVisible" : "suggestSelectInvisible";
+  }),
+
   expertModeClass : Ember.computed("expertMode", function(){
     return this.get("expertMode") === true ? "" : "uk-width-medium-4-6 uk-width-large-3-5";
   }),
