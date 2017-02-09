269604c304d2c2f95639a239fdeed94f8f769c59
Stefan Kracht <stefankracht@gmail.com>
2017-02-09 10:36:00 +0100

    add createdAt and updatedAt

--- app/models/settings.js
+++ app/models/settings.js
@@ -8,5 +8,25 @@ export default DS.Model.extend({
    */
   displayPeterRiegelExlanation: DS.attr('boolean', {
     defaultValue() { return true; }
-  })
+  }),
+
+  /**
+   * createdAt represents the creation date of the settings, will be stored in database
+   * and should be set on create
+   *
+   * @type {Date}
+   */
+  createdAt: DS.attr('date', {
+    defaultValue() { return new Date(); }
+  }),
+
+  /**
+   * updatedAt represents the updating date of the settings, will be stored in database
+   * and should be set on create on on every page visit
+   *
+   * @type {Date}
+   */
+  updatedAt: DS.attr('date', {
+    defaultValue() { return new Date(); }
+  }),
 });
