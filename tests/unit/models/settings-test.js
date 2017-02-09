ca2e4cf2b79353d22d0dd35b5cf83eb6eefcec15
Stefan Kracht <stefankracht@gmail.com>
2017-02-09 10:16:14 +0100

    add displayPeterRiegelExlanation tests

--- tests/unit/models/settings-test.js
+++ tests/unit/models/settings-test.js
@@ -9,3 +9,14 @@ test('settings is a valid ember data Model', function(assert) {
   assert.ok(settings);
   assert.ok(settings instanceof DS.Model);
 });
+
+// displayPeterRiegelExlanation
+test('displayPeterRiegelExlanation is from type Boolean', function(assert) {
+  var settings = this.subject();
+  assert.strictEqual(settings.get("displayPeterRiegelExlanation").constructor.name , "Boolean");
+});
+
+test('displayPeterRiegelExlanation is true by default', function(assert) {
+  var settings = this.subject();
+  assert.strictEqual(settings.get("displayPeterRiegelExlanation") , true);
+});
