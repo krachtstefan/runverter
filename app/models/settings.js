c121b5ba3332297b24690a0c1050f200107bcaf8
Stefan Kracht <stefankracht@gmail.com>
2017-02-09 10:35:00 +0100

    more documentation for displayPeterRiegelExlanation

--- app/models/settings.js
+++ app/models/settings.js
@@ -1,7 +1,10 @@
 import DS from 'ember-data';
 export default DS.Model.extend({
   /**
-   *  Explain the peter riegel limitations to the user if the provides races may not be suitable for the race predictor
+   *  Explain the peter riegel limitations to the user if the provides races may not be
+   *  suitable for the race predictor, will be stored in database
+   *
+   * @type {Boolean}
    */
   displayPeterRiegelExlanation: DS.attr('boolean', {
     defaultValue() { return true; }
