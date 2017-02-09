7a9c46f601d6587cd1999d9f62b9084ff0fbcf66
Stefan Kracht <stefankracht@gmail.com>
2017-02-09 11:57:18 +0100

    remove sticky attribute and change type of riegel explanation

--- app/components/race-predictor.js
+++ app/components/race-predictor.js
@@ -66,9 +66,8 @@ export default Ember.Component.extend({
     if(this.get("prediction.peterRiegelMethodSuitable")===false && this.get("settings.displayPeterRiegelExlanation")===true){
       this.get("flashMessages").add({
         message: this.get('i18n').t("flashMessages.peterRiegelExlanation"),
-        type: 'info',
-        sticky: true,
-        showProgress: true
+        type: 'warning',
+        sticky: true
       });
       this.set("settings.displayPeterRiegelExlanation", "false");
     }
