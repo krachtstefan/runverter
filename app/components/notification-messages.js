96445eb976d506bae3eecfefb6e902b5d655bc2c
Stefan Kracht <stefankracht@gmail.com>
2017-02-16 16:40:37 +0100

    display race predictor release notes

--- app/components/notification-messages.js
+++ app/components/notification-messages.js
@@ -1,5 +1,23 @@
 import Ember from 'ember';
 export default Ember.Component.extend({
   elementId: "notification-container",
-  flashMessages: Ember.inject.service()
+  flashMessages: Ember.inject.service(),
+  i18n: Ember.inject.service(),
+
+  init() {
+    this._super(...arguments);
+    var self = this;
+    if(this.get("settings.displayReleaseNotesRacePredictor")===true){
+      this.set("settings.displayReleaseNotesRacePredictor", false);
+      this.get("flashMessages").add({
+        message: this.get('i18n').t("flashMessages.releaseNotesRacePredictor"),
+        type: 'info',
+        sticky: true,
+        onDestroy() {
+          self.get("settings").save();
+        }
+      });
+    }
+  },
+
 });
