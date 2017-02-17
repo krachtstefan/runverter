2d3fc351f630492f5a196f79b87788bbd6c235fd
Stefan Kracht <stefankracht@gmail.com>
2017-02-17 15:04:00 +0100

    remove race predictor release notes when entering the component

--- app/components/notification-messages.js
+++ app/components/notification-messages.js
@@ -21,4 +21,16 @@ export default Ember.Component.extend({
     }
   },
 
+  clearReleaseNotesRacePredictor : Ember.on("init", Ember.observer('selectedMenuItem', function(){
+    // when entering race predictor component, find the race predictor announcement notification and remove it
+    // also make sure to not display it again
+    if(this.get("selectedMenuItem.key")==="rp"){
+      var relaseNotesMessage = this.get("flashMessages.queue").filterBy('identifier', "releaseNotesRacePredictor")
+      if(relaseNotesMessage.length > 0){
+        relaseNotesMessage[0].destroyMessage();
+      }
+      this.set("settings.displayReleaseNotesRacePredictor", false);
+      this.get("settings").save();
+    }
+  })),
 });
