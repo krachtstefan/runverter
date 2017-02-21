64c844446f8fa048cb3f4ac486f623e7d3cb5198
Stefan Kracht <stefankracht@gmail.com>
2017-02-21 16:57:49 +0100

    add some more specific run factories

--- tests/factories/run.js
+++ tests/factories/run.js
@@ -1,2 +1,9 @@
 import FactoryGuy from 'ember-data-factory-guy';
-FactoryGuy.define('run', {});
+FactoryGuy.define('run', {
+  sub4Marathon: {
+    timeSec : new BigNumber(14399), lengthM : new BigNumber(42195)
+  },
+  sub2HalfMarathon: {
+    timeSec : new BigNumber(7199), lengthM : new BigNumber(21097.5)
+  }
+});
