b27fa5ba05196741a92a577bcfddd8b390f4f28d
Stefan Kracht <stefankracht@gmail.com>
2017-02-21 17:30:10 +0100

    remove unsed variable definition

--- tests/integration/components/control-panel-test.js
+++ tests/integration/components/control-panel-test.js
@@ -1,18 +1,11 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { make, manualSetup }  from 'ember-data-factory-guy';
 
 moduleForComponent('control-panel', 'Integration | Component | control panel', {
-  integration: true,
-  beforeEach: function () {
-    manualSetup(this.container);
-  }
+  integration: true
 });
 
 test('it renders', function(assert) {
-  this.set('run', make('run'));
-
   this.render(hbs`{{control-panel}}`);
-
   assert.equal(this.$('div').attr('class'), 'control-panel ember-view', 'wrapper container found');
 });
--- tests/integration/components/notification-messages-test.js
+++ tests/integration/components/notification-messages-test.js
@@ -1,18 +1,11 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { make, manualSetup }  from 'ember-data-factory-guy';
 
 moduleForComponent('notification-messages', 'Integration | Component | notification messages', {
-  integration: true,
-  beforeEach: function () {
-    manualSetup(this.container);
-  }
+  integration: true
 });
 
 test('it renders', function(assert) {
-  this.set('run', make('run'));
-
   this.render(hbs`{{notification-messages}}`);
-
   assert.equal(this.$('div').attr('id'), 'notification-container', 'wrapper container found');
 });
