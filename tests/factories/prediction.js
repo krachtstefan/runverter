c5153644789827dd490013e18463d2230a160c00
Stefan Kracht <stefankracht@gmail.com>
2017-02-23 11:09:22 +0100

    define predicted and achieved run for prediction factory

--- tests/factories/prediction.js
+++ tests/factories/prediction.js
@@ -1,2 +1,5 @@
 import FactoryGuy from 'ember-data-factory-guy';
-FactoryGuy.define('prediction', {});
+FactoryGuy.define('prediction', {
+  predictedRun: FactoryGuy.belongsTo('sub2Marathon'),
+  achievedRun: FactoryGuy.belongsTo('sub4Marathon')
+});
