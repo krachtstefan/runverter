9ecfa8ce2c1e7cf1b3269f5248b08a6907944d79
Stefan Kracht <stefankracht@gmail.com>
2017-03-22 14:21:31 +0100

    add function to foce digits helper

--- app/helpers/force-digits.js
+++ app/helpers/force-digits.js
@@ -1,7 +1,27 @@
 import Ember from 'ember';
+/**
+  * Force input string to have a certain length. If the required length is not fullfilled
+  * this method will change the string to achieve the required lenght by pre- or appending
+  * a certain filler string. This is usefull to add leading leading zeros to a numeric string
+  *
+  * @param  {string} params the params of the helper, not needed in this one
+  * @param  {Object} args all arguments delivered to the helper
+  * @param {string} args.value the value that might be filled
+  * @param {string} args.mode filling mode, might be append or prepend, append by default
+  * @param {string|number} args.length the minimum lenght that the output string will have, 2 by default
+  * @param {string|number} args.filler the string that is used to fill, 0 by default
+  * @return {string} output string
+  */
+export function forceDigits(params, args) {
+  var output = String(args.value);
+  let mode = ["append", "prepend"].indexOf(args.mode) >= 0 ? args.mode : "append";
+  let length = args.length === undefined ? 2 : parseInt(args.length);
+  let filler = args.filler === undefined ? "0" : args.filler;
 
-export function forceDigits(params/*, hash*/) {
-  return params;
+  for (var i = args.value.length; i < length; i++) {
+    output = mode === "append" ? output+filler : filler+output;
+  }
+  return output;
 }
 
 export default Ember.Helper.helper(forceDigits);
