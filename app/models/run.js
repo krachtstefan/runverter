4d5096cbb39aa172d71e5d23e5ccc61bac5bb9c7
Stefan Kracht <stefankracht@gmail.com>
2017-03-27 14:49:18 +0200

    use bignumber.js syntax for comparsion

--- app/models/run.js
+++ app/models/run.js
@@ -1510,8 +1510,8 @@ export default DS.Model.extend({
     if(splitCountCeiled.greaterThan(1) === true){
       for (let i = 1; splitCountCeiled.greaterThanOrEqualTo(i); i++) {
         var thisSplitDistance = splitCountCeiled.equals(i) ? lastSplitDistance : splitDistance; // different length for last split
-        var currentSplittingStrategy = (i <= turningPoint) ? splittingStrategy : splittingStrategySecondHalf; // splitting strategy of the current split
 
+        var currentSplittingStrategy = turningPoint.greaterThan(i) ? splittingStrategy : splittingStrategySecondHalf; // splitting strategy of the current split
         var thisSplitTime = splitCountCeiled.equals(i) ? lastSplitTime : splitTime; // different time for last split
         thisSplitTime = thisSplitTime.plus(thisSplitTime.times(currentSplittingStrategy).dividedBy(100)); // apply splitting strategy to
 
