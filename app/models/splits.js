4aa0d6b2f22d188d1ddb9f12269f6bf1a8e18247
Stefan Kracht <stefankracht@gmail.com>
2017-04-03 11:31:17 +0200

    add timestampt to splits model

--- app/models/splits.js
+++ app/models/splits.js
@@ -2,4 +2,33 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 
+  /**
+   * createdAt represents the creation date of the splits, will be stored in database
+   * and should be set on create
+   *
+   * @type {Date}
+   */
+  createdAt: DS.attr('date', {
+    defaultValue() { return new Date(); }
+  }),
+
+  /**
+   * updatedAt represents the updating date of the splits, will be stored in database
+   * and should be set on create on on every page visit
+   *
+   * @type {Date}
+   */
+  updatedAt: DS.attr('date', {
+    defaultValue() { return new Date(); }
+  }),
+
+  run: DS.belongsTo('run');
+
+  /**
+   * update updatedAt before saving the settings
+   */
+  save: function(){
+    this.set("updatedAt", new Date());
+    this._super(...arguments);
+  },
 });
