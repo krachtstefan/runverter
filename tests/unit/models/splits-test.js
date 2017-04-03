c998159fb01180cb7f553d930d45a8e781df100e
Stefan Kracht <stefankracht@gmail.com>
2017-04-03 16:56:00 +0200

    add basic splits tests

--- tests/unit/models/splits-test.js
+++ tests/unit/models/splits-test.js
@@ -1,12 +1,37 @@
+import DS from 'ember-data';
+// import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('splits', 'Unit | Model | splits', {
-  // Specify the other units that are required for this test.
-  needs: []
+moduleForModel('splits', 'Splits Model', {
+  needs: ['model:run']
 });
 
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+test('splits is a valid ember data Model', function(assert) {
+  var splits = this.subject();
+  assert.ok(splits);
+  assert.ok(splits instanceof DS.Model);
+});
+
+// createdAt
+test('createdAt is from type Date', function(assert) {
+  var splits = this.subject();
+  assert.strictEqual(splits.get("createdAt").constructor.name , "Date");
+});
+
+test('createdAt equals current date', function(assert) {
+  var splits = this.subject();
+  var today = new Date();
+  assert.strictEqual(splits.get("createdAt").toString() , today.toString());
+});
+
+// updatedAt
+test('updatedAt is from type Date', function(assert) {
+  var splits = this.subject();
+  assert.strictEqual(splits.get("createdAt").constructor.name , "Date");
+});
+
+test('updatedAt equals current date', function(assert) {
+  var splits = this.subject();
+  var today = new Date();
+  assert.strictEqual(splits.get("updatedAt").toString() , today.toString());
 });
