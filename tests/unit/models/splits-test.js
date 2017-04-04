ac1ec361bf864af9d517037593b0b94be2c90d51
Stefan Kracht <stefankracht@gmail.com>
2017-04-04 14:27:21 +0200

    add run tests

--- tests/unit/models/splits-test.js
+++ tests/unit/models/splits-test.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
-// import Ember from 'ember';
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('splits', 'Splits Model', {
@@ -13,6 +13,14 @@ test('splits is a valid ember data Model', function(assert) {
 });
 
 // run
+test('run is a relation to run model', function(assert) {
+  const splits = this.store().modelFor('splits');
+  const relationship = Ember.get(splits, 'relationshipsByName').get('run');
+  assert.equal(relationship.key, 'run');
+  assert.equal(relationship.kind, 'belongsTo');
+  assert.equal(relationship.type, 'run');
+});
+
 // splitDistance
 // splittingStrategy
 // splittingStrategySecondHalf
