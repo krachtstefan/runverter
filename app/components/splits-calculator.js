f7ee3cd6ac879e144186fd1ba8e0fea16c75fc6c
Stefan Kracht <stefankracht@gmail.com>
2017-04-09 10:43:44 +0200

    initial chart

--- app/components/splits-calculator.js
+++ app/components/splits-calculator.js
@@ -29,6 +29,39 @@ export default Ember.Component.extend({
     return 'ontouchstart' in document.documentElement;
   }),
 
+  chartOption : {},
+
+  chartData : Ember.computed("run.splits.splits", function(){
+    var data = {
+      labels: this.get("run.splits.splits").map(function(data) { return data.get("run.lengthKm").round(2).toNumber() }),
+      datasets: [
+        {
+          label: "My First dataset",
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data : this.get("run.splits.splits").map(function(data) { return data.get("split.paceMinPerKm").toNumber() }),
+          spanGaps: false,
+        }
+      ]
+    }
+    return data;
+  }),
+
   targetTimesSuggestions  : Ember.computed("run.lengthM", "i18n.locale", function(){
     var self = this;
     return this.get("targetTimes.templates").filter(function(item) {
