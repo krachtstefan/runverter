bb89f6766d69303f713f65e866d32ed993a89d90
Stefan Kracht <stefankracht@gmail.com>
2017-04-27 09:54:36 +0200

    PhantomJS is working fine again
    maybe due to a ember-cli update?

--- testem.js
+++ testem.js
@@ -2,18 +2,11 @@
 module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
-  "launchers": {
-    "AltPhantom": {
-      "exe": "phantomjs",
-      "args": ["tests/phantom-runner.js"],
-      "protocol": "browser"
-    }
-  },
   "launch_in_ci": [
-    "AltPhantom"
+    "PhantomJS"
   ],
   "launch_in_dev": [
-    "AltPhantom",
+    "PhantomJS",
     "Chrome"
   ]
 };
