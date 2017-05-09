d1ea33f545d9fa90bbe66467514c6c5e69522d11
Stefan Kracht <stefankracht@gmail.com>
2017-05-09 08:26:53 +0200

    remove splits calculator release notes

--- app/components/notification-messages.js
+++ app/components/notification-messages.js
@@ -2,31 +2,5 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   elementId: "notification-container",
   notifications: Ember.inject.service('notification-messages'),
-  i18n: Ember.inject.service(),
-
-  init() {
-    this._super(...arguments);
-    var self = this;
-    if(this.get("settings.displayReleaseNotesSplitsCalculator")===true){
-      this.set("settings.displayReleaseNotesSplitsCalculator", false);
-      this.get('notifications').success(this.get('i18n').t("flashMessages.releaseNotesSplitsCalculator"), {
-        onClick() {
-          self.get("settings").save();
-        }
-      });
-    }
-  },
-
-  clearReleaseNotesRacePredictor : Ember.on("init", Ember.observer('selectedMenuItem', function(){
-    // when entering splits calculator component, find the splits calculator announcement notification and remove it
-    // also make sure to not display it again
-    if(this.get("selectedMenuItem.key")==="sc"){
-      var relaseNotesMessage = this.get("notifications.content").filterBy('message.string', this.get('i18n').t("flashMessages.releaseNotesSplitsCalculator").string);
-      if(relaseNotesMessage.length > 0){
-        this.get('notifications').removeNotification(relaseNotesMessage[0]);
-        this.set("settings.displayReleaseNotesSplitsCalculator", false);
-        this.get("settings").save();
-      }
-    }
-  })),
+  i18n: Ember.inject.service()
 });
