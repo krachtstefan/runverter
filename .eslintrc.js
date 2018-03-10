ef70b8ad7c608e9075935323cf270c82a4f363e6
Stefan Kracht <stefankracht@gmail.com>
2018-03-10 14:33:27 +0100

    allow avoid-leaking-state-in-ember-objects

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
     browser: true
   },
   rules: {
+    // required to make it possible to set BigNumber objects as default attribute values
+    'ember/avoid-leaking-state-in-ember-objects': 'off'
   },
   overrides: [
     // node files
