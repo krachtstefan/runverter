eec9012f99003e2d8d79ffa762b3a20a9793bd86
Stefan Kracht <stefankracht@gmail.com>
2018-03-10 14:59:50 +0100

    ember init

--- app/app.js
+++ app/app.js
@@ -3,9 +3,7 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-let Runverter;
-
-Runverter = Application.extend({
+const Runverter = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
