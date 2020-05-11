aa2d6f2f7c8838983b051d1a9bc95bd761d7d6a4
Stefan Kracht <stefankracht@gmail.com>
2020-05-11 21:32:00 +0200

    remove old comment

--- config/deploy.js
+++ config/deploy.js
@@ -1,6 +1,5 @@
 const accessKeyId = process.env.RUNVERTER_AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.RUNVERTER_AWS_SECRET_ACCESS_KEY;
-
 const sharedConfig = {
   'html-manifest': {
     filename: 'manifest.appcache',
@@ -54,11 +53,5 @@ module.exports = function (deployTarget) {
   if (Object.keys(deployConfig).includes(deployTarget) === false) {
     throw new Error('Invalid deployTarget ' + deployTarget);
   }
-
-  // TRY TO UPLOAD a hashed manifest, if this does not work add a clean cache deploy routine that always overwrites the one appcache
-
-  // use s3 upload twice
-  // http://ember-cli-deploy.com/docs/v1.0.x/including-a-plugin-twice/
-
   return deployConfig[deployTarget];
 };
