da503db20d357616fd02cffcc07b2003732f6407
Stefan Kracht <stefankracht@gmail.com>
2020-05-11 21:08:05 +0200

    working deploy

--- config/deploy.js
+++ config/deploy.js
@@ -1,58 +1,64 @@
-const deployConfig = {
-  production: {
-    s3KeyId: process.env.RUNVERTER_AWS_ACCESS_KEY_ID,
-    s3Key: process.env.RUNVERTER_AWS_SECRET_ACCESS_KEY,
-    mainBucket: {
-      name: 'staging.runverter.io',
-      region: 'eu-central-1',
-    },
-    assetUrl: 'http://s.runverter.io/',
-    assetBucket: {
-      name: 's.runverter.io',
-      region: 'eu-central-1',
-    },
+const accessKeyId = process.env.RUNVERTER_AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.RUNVERTER_AWS_SECRET_ACCESS_KEY;
+
+const sharedConfig = {
+  'html-manifest': {
+    filename: 'manifest.appcache',
+    manifestRoot: '',
+    prependPath: 'http://s.runverter.io/',
+    excludePaths: ['index.html'],
+    includePaths: [
+      'https://fonts.googleapis.com/css?family=Open+Sans:400,700',
+      'https://fonts.gstatic.com/s/opensans/v13/cJZKeOuBrn4kERxqtaUH3aCWcynf_cDxXwCLxiixG1c.ttf',
+    ],
+    network: ['*'],
   },
 };
 
-// eventuell app cache nach dem aktivieren hochladen
-// http://ember-cli-deploy.com/docs/v1.0.x/pipeline-hooks/
-// update docs
+const deployConfig = {
+  production: {
+    's3-index': {
+      accessKeyId,
+      secretAccessKey,
+      bucket: 'staging.runverter.io',
+      region: 'eu-central-1',
+    },
+    ['s3']: {
+      accessKeyId,
+      secretAccessKey,
+      bucket: 's.runverter.io',
+      region: 'eu-central-1',
+      filePattern:
+        '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,otf,appcache,json}',
+    },
+    ...sharedConfig,
+  },
+  appcache: {
+    ['s3']: {
+      accessKeyId,
+      secretAccessKey,
+      bucket: 'staging.runverter.io',
+      region: 'eu-central-1',
+      filePattern: '**/*.appcache', // only upload .appcache
+    },
+    pipeline: {
+      disabled: {
+        allExcept: ['build', 'gzip', 'html-manifest', 's3'],
+      },
+    },
+    ...sharedConfig,
+  },
+};
+
 module.exports = function (deployTarget) {
   if (Object.keys(deployConfig).includes(deployTarget) === false) {
     throw new Error('Invalid deployTarget ' + deployTarget);
   }
-  const { s3KeyId, s3Key, mainBucket, assetBucket, assetUrl } = deployConfig[
-    deployTarget
-  ];
 
-  console.log('🎉', mainBucket.name, mainBucket.region);
-  var ENV = {
-    's3-index': {
-      accessKeyId: s3KeyId,
-      secretAccessKey: s3Key,
-      bucket: mainBucket.name,
-      region: mainBucket.region,
-    },
-    s3: {
-      accessKeyId: s3KeyId,
-      secretAccessKey: s3Key,
-      bucket: assetBucket.name,
-      region: assetBucket.region,
-      filePattern:
-        '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,otf,appcache,json}',
-    },
-    'html-manifest': {
-      filename: 'manifest.appcache',
-      manifestRoot: '/',
-      prependPath: assetUrl,
-      excludePaths: ['index.html'],
-      includePaths: [
-        'https://fonts.googleapis.com/css?family=Open+Sans:400,700',
-        'https://fonts.gstatic.com/s/opensans/v13/cJZKeOuBrn4kERxqtaUH3aCWcynf_cDxXwCLxiixG1c.ttf',
-      ],
-      network: ['*'],
-    },
-  };
+  // TRY TO UPLOAD a hashed manifest, if this does not work add a clean cache deploy routine that always overwrites the one appcache
 
-  return ENV;
+  // use s3 upload twice
+  // http://ember-cli-deploy.com/docs/v1.0.x/including-a-plugin-twice/
+
+  return deployConfig[deployTarget];
 };
