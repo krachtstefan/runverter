cbfbc53154cdbb647bb3f018a03f747f7fd110ce
Stefan Kracht <stefankracht@gmail.com>
2022-04-19 11:20:41 +0200

    switch ga tag agains gtag in eslint globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,16 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  plugins: ['ember'],
+  extends: ['eslint:recommended', 'plugin:ember/recommended'],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
     // required to make it possible to set BigNumber objects as default attribute values
-    'ember/avoid-leaking-state-in-ember-objects': 'off'
+    'ember/avoid-leaking-state-in-ember-objects': 'off',
   },
   overrides: [
     // node files
@@ -25,22 +20,22 @@ module.exports = {
         'testem.js',
         'ember-cli-build.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
       ],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015
+        ecmaVersion: 2015,
       },
       env: {
         browser: false,
-        node: true
-      }
-    }
+        node: true,
+      },
+    },
   ],
   globals: {
-    "require" : true,
-    "BigNumber" : true,
-    "Decimal" : true,
-    "ga" : true
-  }
+    require: true,
+    BigNumber: true,
+    Decimal: true,
+    gtag: true,
+  },
 };
